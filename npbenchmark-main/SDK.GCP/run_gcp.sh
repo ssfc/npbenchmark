23798cf29e45c20a3aedf344b50cd8c5bf62d715
YE GAO <sortsimilar@gmail.com>
2022-11-28 22:44:55 +0800

    Update run_gcp.sh

--- npbenchmark-main/SDK.GCP/run_gcp.sh
+++ npbenchmark-main/SDK.GCP/run_gcp.sh
@@ -4,10 +4,12 @@ g++ -static-libgcc -static-libstdc++ tabucol.cpp GraphColoring.cpp Main.cpp -O3
 #./a.out 999999 4 <./data/DSJC0250.9.txt > sln.250.9.txt
 #./a.out 999999 5 <./data/DSJC0250.9.txt > sln.250.9.txt
 
+data_path="./data/DSJC0250.9.txt"
+
 for (( i=1; i<=$1; i++ ))
 do
     echo "Random seed: $i "
-    ./a.out 999999 "$i" <./data/DSJC0250.9.txt > sln.250.9.txt
+    ./a.out 999999 "$i" < $data_path > sln.250.9.txt
 done
 
 
