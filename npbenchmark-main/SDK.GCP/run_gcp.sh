7c5d292b79f36fab1f72b6905ca0c8728fbaaf99
YE GAO <sortsimilar@gmail.com>
2022-11-28 16:52:02 +0800

    add parameters

--- npbenchmark-main/SDK.GCP/run_gcp.sh
+++ npbenchmark-main/SDK.GCP/run_gcp.sh
@@ -4,7 +4,7 @@ g++ -static-libgcc -static-libstdc++ tabucol.cpp GraphColoring.cpp Main.cpp -O3
 #./a.out 999999 4 <./data/DSJC0250.9.txt > sln.250.9.txt
 #./a.out 999999 5 <./data/DSJC0250.9.txt > sln.250.9.txt
 
-for (( i=1; i<=5; i++ ))
+for (( i=1; i<=$1; i++ ))
 do
     echo "Random seed: $i "
     ./a.out 999999 "$i" <./data/DSJC0250.9.txt > sln.250.9.txt
