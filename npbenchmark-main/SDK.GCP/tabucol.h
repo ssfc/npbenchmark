7a7fda3932cca1d17fd2f2d7ca8082c3d8a2640c
YE GAO <sortsimilar@gmail.com>
2022-11-30 22:29:26 +0800

    Update tabucol.h

--- npbenchmark-main/SDK.GCP/tabucol.h
+++ npbenchmark-main/SDK.GCP/tabucol.h
@@ -24,7 +24,7 @@ private:
     unsigned int* solution; // color of each vertex; dimension, num_vertex;
     unsigned int conflict;
 
-    unsigned int** tabu_tenure_table; // dimension, num_vertex * num_color;
+    unsigned int** tabu_tenure_table; // tenure of each color on each node; dimension, num_vertex * num_color;
     int** adj_color_table; // first dim is num vertex, second dim is num color;
 
     int delta; // ÒÆ¶¯ÔöÁ¿
