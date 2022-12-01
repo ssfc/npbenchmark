4f1537eb97b57b1d7e6a76db4b98a6a5479d023d
YE GAO <sortsimilar@gmail.com>
2022-12-01 10:36:22 +0800

    Update tabucol.cpp

--- npbenchmark-main/SDK.GCP/tabucol.cpp
+++ npbenchmark-main/SDK.GCP/tabucol.cpp
@@ -76,7 +76,7 @@ Graph::Graph(int input_num_vertex, int input_edge_num, int input_num_color, vect
         equ_delta = new int* [2000];
         for(int i=0;i<2000;i++)
         {
-            equ_delta[i] = new int[2]; 
+            equ_delta[i] = new int[2];
         }
 
         for (int i = 0; i < num_vertex; i++)
