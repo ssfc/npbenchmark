7eb4ecffcb7853b62a4faee0a3fc46e49b7394a5
YE GAO <sortsimilar@gmail.com>
2022-12-07 10:27:59 +0800

    Update tabucol.cpp

--- npbenchmark-main/SDK.GCP/tabucol.cpp
+++ npbenchmark-main/SDK.GCP/tabucol.cpp
@@ -217,7 +217,7 @@ void Graph::find_move()
                             }
 
                             equal_nontabu_delta[count][0] = i; // i is vertex;
-                            equal_nontabu_delta[count][1] = j; // j is color; 
+                            equal_nontabu_delta[count][1] = j; // j is color;
 
                             count++;
                         }
@@ -232,8 +232,8 @@ void Graph::find_move()
                                 tabu_count = 0;
                             }
 
-                            equal_tabu_delta[tabu_count][0] = i;
-                            equal_tabu_delta[tabu_count][1] = j;
+                            equal_tabu_delta[tabu_count][0] = i; // i is vertex;
+                            equal_tabu_delta[tabu_count][1] = j; // j is color; 
 
                             tabu_count++;
                         }
