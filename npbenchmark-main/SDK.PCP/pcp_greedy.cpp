bbbbb1638c08a0cf75313ae898448fae788d6d1f
YE GAO <sortsimilar@gmail.com>
2022-12-10 18:22:03 +0800

    Update pcp_greedy.cpp

--- npbenchmark-main/SDK.PCP/pcp_greedy.cpp
+++ npbenchmark-main/SDK.PCP/pcp_greedy.cpp
@@ -3,6 +3,13 @@
 //
 # include "pcp_greedy.h"
 
+#include <random>
+
+using namespace std;
+
+mt19937 pseudoRandNumGen;
+void initRand(int seed) { pseudoRandNumGen = mt19937(seed); }
+
 PCP_Greedy::PCP_Greedy(int input_center_num, int input_num_color, vector<vector<int>> &input_coverages, int seed)
 {
 
