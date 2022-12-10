48e09b655052644755ed0352e02d38168b83544d
YE GAO <sortsimilar@gmail.com>
2022-12-10 16:34:11 +0800

    constructor and destructor

--- npbenchmark-main/SDK.PCP/pcp_greedy.cpp
+++ npbenchmark-main/SDK.PCP/pcp_greedy.cpp
@@ -1,3 +1,20 @@
 //
 // Created by take_ on 2022/12/10.
 //
+# include "pcp_greedy.h"
+
+PCP_Greedy::PCP_Greedy(int input_center_num, int input_num_color, vector<vector<int>> &input_coverages, int seed)
+{
+
+}
+
+PCP_Greedy::~PCP_Greedy()
+{
+
+}
+
+
+
+
+
+
--- npbenchmark-main/SDK.PCP/pcp_greedy.h
+++ npbenchmark-main/SDK.PCP/pcp_greedy.h
@@ -5,9 +5,15 @@
 #ifndef SDK_PCP_PCP_GREEDY_H
 #define SDK_PCP_PCP_GREEDY_H
 
+# include <vector>
+
+using namespace std;
+
 class PCP_Greedy
 {
-    
+public:
+    PCP_Greedy(int input_center_num, int input_num_color, vector<vector<int>>& input_coverages, int seed);
+    ~PCP_Greedy();
 };
 
 #endif //SDK_PCP_PCP_GREEDY_H
