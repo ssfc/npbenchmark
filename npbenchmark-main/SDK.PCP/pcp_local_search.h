371f35d16f813979d9b5e363ea6a7573180b451c
YE GAO <sortsimilar@gmail.com>
2022-12-11 15:25:55 +0800

    Update pcp_local_search.h

--- npbenchmark-main/SDK.PCP/pcp_local_search.h
+++ npbenchmark-main/SDK.PCP/pcp_local_search.h
@@ -11,14 +11,18 @@ using namespace std;
 
 class PCP_Local_Search
 {
-private:
+private: // member variables.
     int num_node;
     int num_center;
     vector<vector<int>> center_coverages;
     vector<vector<int>> nodes_with_drops;
 
     unsigned int* solution; // index of selected center; dimension, num_center;
-public:
+    vector<int> universe; // all points;
+    vector<int> covered; // points already covered by set;
+    vector<int> uncovered; // points has not been covered by set;
+
+public: // member functions. 
     PCP_Local_Search(int input_nodeNum, int input_centerNum, vector<vector<int>>& input_coverages, vector<vector<int>> &input_nodesWithDrops, int seed);
     ~PCP_Local_Search();
 
