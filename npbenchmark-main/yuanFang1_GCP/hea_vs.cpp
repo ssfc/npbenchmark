1d58ce78769694a9147eb6c665b79604368877c3
YE GAO <sortsimilar@gmail.com>
2023-01-01 20:33:16 +0800

    Update hea_vs.cpp

--- npbenchmark-main/yuanFang1_GCP/hea_vs.cpp
+++ npbenchmark-main/yuanFang1_GCP/hea_vs.cpp
@@ -4,11 +4,11 @@
 // hea_vs.cpp : 定义控制台应用程序的入口点。
 //
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
 #include <string>
-#include <string.h>
+#include <cstring>
 #include <iostream>
 #include <fstream>
 
