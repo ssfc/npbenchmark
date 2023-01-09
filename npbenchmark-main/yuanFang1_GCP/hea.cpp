ef9a6285913894b3d0b2ed5c85f7ec42383ed2b1
YE GAO <sortsimilar@gmail.com>
2023-01-09 10:45:21 +0800

    Update hea.cpp

--- npbenchmark-main/yuanFang1_GCP/hea.cpp
+++ npbenchmark-main/yuanFang1_GCP/hea.cpp
@@ -66,9 +66,6 @@ void add_conflict(int adj_vertex);
 void delete_conflict(int adj_vertex);
 void cross_over(int p1, int p2, int *index1);
 
-
-
-
 void Hybrid_Evolution::insert_adj_list(int i, int j)
 {
     ArcNode *temp1 = (ArcNode *)malloc(sizeof(ArcNode));
@@ -77,187 +74,7 @@ void Hybrid_Evolution::insert_adj_list(int i, int j)
     adj_list[j].first = temp1;
 }
 
-/*
-argv[1]:文件名
-argv[2]:颜色数
-*/
-int main(int argc, char *argv[])
-{
-    Hybrid_Evolution test;
-
-
-
-    int i, j, p;
-    char c, s1[100], s2[100], file[100];
-    FILE *fp;
-    srand(6);
-
-    sprintf(file, "./instances/%s", argv[1]);
-
-    if ((fp = fopen(file, "r")) == nullptr)
-    {
-        cout << "file not open";
-        return 0;
-    }
-
-    do
-    {
-        fgets(s1, 100, fp);
-    } while (s1[0] == 'c');
-
-    sscanf(s1, "%c %s %d %d", &c, s2, &num_vertex, &num_edge);
-
-    // allocate space to variables;
-    dynamic_alloc();
-
-    memset(adj_list, 0, sizeof(VerNode)*(num_vertex + 1));
-
-    while (!feof(fp))
-    {
-        fscanf(fp, "%c %d %d\n", &c, &i, &j);
-        test.insert_adj_list(i, j);
-        test.insert_adj_list(j, i);
-    }
-
-    population.min_conflict = INT_MAX;
-    num_color = atoi(argv[2]);
-
-    // this is also the process of initialization;
-    for (p = 0; p < num_population; p++)
-    {
-        memset(adj_color_table, 0, sizeof(adj_color_table));
-        memset(tabu_tenure_table, 0, sizeof(tabu_tenure_table));
-        f = best_conflict = conflict_num = 0;
-
-        // initialization: set random solution to each solution in the population;
-        for (i = 1; i <= num_vertex; i++)
-        {
-            solution_collection[p][i] = rand() % num_color;
-            //cout << solution[i] <<' ';
-        }
-
-        // do tabu-search for each population in the collection;
-        // cout << "Compute conflict is: " << compute_conflict(solution_collection[p]) << endl;
-        tabu_search(solution_collection[p]);
-
-        population.num_conflict[p] = f;
-
-        // record the min conflict up till now;
-        if (f < population.min_conflict)
-        {
-            population.min_conflict = f;
-            population.min_conflict_index = p;
-        }
-
-        if (f == 0)
-            break;
-    }
-
-    memset(population_solution, 0, sizeof(population_solution));
-    double start_time = clock();
-
-    for (p = 0; p < num_population; p++)
-    {
-        for (i = 1; i <= num_vertex; i++)
-        {
-            int color = population_solution[p].index1[i] = solution_collection[p][i]; // color of population p, vertex i;
-            int color_num = population_solution[p].color_num[color];
-            population_solution[p].psol[color][color_num] = i;
-            population_solution[p].index2[i] = color_num++;
-            population_solution[p].color_num[color] = color_num;
-        }
-    }
-
-    Population_solution temps;
-
-    int population_iteration = 0;
-    while (population.min_conflict != 0)
-    {
-        // random select two index from population as parents;
-        int p1 = rand() % num_population, p2;
-
-        do
-        {
-            p2 = rand() % num_population;
-        } while (p1 == p2);
-
-        memset(&temps, 0, sizeof(temps));
-
-        cross_over(p1, p2, temps.index1);
-
-        // reset adj_color_table and tabu_tenure_table to zero;
-        memset(adj_color_table, 0, sizeof(adj_color_table));
-        memset(tabu_tenure_table, 0, sizeof(tabu_tenure_table));
-        f = best_conflict = conflict_num =0;
-
-        tabu_search(temps.index1); // 仅仅需要对新形成的temps进行禁忌搜索;
-
-        for (i = 1; i <= num_vertex; i++)
-        {//变成划分的形式
-            int color = temps.index1[i];
-            int color_num = temps.color_num[color];
-            temps.psol[color][color_num] = i;
-            temps.index2[i] = color_num;
-            temps.color_num[color] = ++color_num;
-        }
-
-        int max_conflict = -1, max_conflict_index;
-
-        for (i = 0; i < num_population; i++)
-        {
-            if (population.num_conflict[i] > max_conflict)
-            {
-                max_conflict = population.num_conflict[i];
-                max_conflict_index = i;
-            }
-        }
-
-        population_solution[max_conflict_index] = temps; // 将种群中冲突数最大的替换成temps
-        population.num_conflict[max_conflict_index] = f;
-
-        if (f < population.min_conflict)
-        {
-            population.min_conflict = f;
-            population.min_conflict_index = max_conflict_index;
-        }
-
-        cout << "min conflict = " << population.min_conflict << endl;
-        cout << "min conflict index = " << population.min_conflict_index << endl;
-        population_iteration++;
-    }
-
-    double end_time = clock();
-    double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
-    cout << "Population iteration: " << population_iteration << endl;
-    cout << "elapsed time(s): " << elapsed_time << endl;
-
-    if (population.min_conflict == 0)
-    {
-        fp = fopen("result.txt", "a+");
-        if (fp == nullptr)
-            printf("output file open error\n");
-        fprintf(fp, "%s %-9s %-15lf %-7d\n", argv[1], argv[2], elapsed_time , max_iter);
-
-        cout << "color of each vertex: ";
-        for(int i=1;i<=num_vertex;i++)
-        {
-            cout << population_solution[population.min_conflict_index].index1[i] << " ";
-        }
-        cout << endl;
-
-        cout << "conflict of solution 19: ";
-        cout << test.compute_conflict(population_solution[19].index1) << endl;
-
-        cout << "conflict of final solution: ";
-        cout << test.compute_conflict(population_solution[population.min_conflict_index].index1) << endl;
-    }
-    else
-        cout << "over time" << endl;
-
-    return 0;
-}
-
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void dynamic_alloc()
 {
@@ -519,4 +336,184 @@ int Hybrid_Evolution::compute_conflict(int *solution)
     this_conflict = this_conflict / 2;
 
     return this_conflict;
+}
+
+/*
+argv[1]:文件名
+argv[2]:颜色数
+*/
+int main(int argc, char *argv[])
+{
+    Hybrid_Evolution test;
+
+
+
+    int i, j, p;
+    char c, s1[100], s2[100], file[100];
+    FILE *fp;
+    srand(6);
+
+    sprintf(file, "./instances/%s", argv[1]);
+
+    if ((fp = fopen(file, "r")) == nullptr)
+    {
+        cout << "file not open";
+        return 0;
+    }
+
+    do
+    {
+        fgets(s1, 100, fp);
+    } while (s1[0] == 'c');
+
+    sscanf(s1, "%c %s %d %d", &c, s2, &num_vertex, &num_edge);
+
+    // allocate space to variables;
+    dynamic_alloc();
+
+    memset(adj_list, 0, sizeof(VerNode)*(num_vertex + 1));
+
+    while (!feof(fp))
+    {
+        fscanf(fp, "%c %d %d\n", &c, &i, &j);
+        test.insert_adj_list(i, j);
+        test.insert_adj_list(j, i);
+    }
+
+    population.min_conflict = INT_MAX;
+    num_color = atoi(argv[2]);
+
+    // this is also the process of initialization;
+    for (p = 0; p < num_population; p++)
+    {
+        memset(adj_color_table, 0, sizeof(adj_color_table));
+        memset(tabu_tenure_table, 0, sizeof(tabu_tenure_table));
+        f = best_conflict = conflict_num = 0;
+
+        // initialization: set random solution to each solution in the population;
+        for (i = 1; i <= num_vertex; i++)
+        {
+            solution_collection[p][i] = rand() % num_color;
+            //cout << solution[i] <<' ';
+        }
+
+        // do tabu-search for each population in the collection;
+        // cout << "Compute conflict is: " << compute_conflict(solution_collection[p]) << endl;
+        tabu_search(solution_collection[p]);
+
+        population.num_conflict[p] = f;
+
+        // record the min conflict up till now;
+        if (f < population.min_conflict)
+        {
+            population.min_conflict = f;
+            population.min_conflict_index = p;
+        }
+
+        if (f == 0)
+            break;
+    }
+
+    memset(population_solution, 0, sizeof(population_solution));
+    double start_time = clock();
+
+    for (p = 0; p < num_population; p++)
+    {
+        for (i = 1; i <= num_vertex; i++)
+        {
+            int color = population_solution[p].index1[i] = solution_collection[p][i]; // color of population p, vertex i;
+            int color_num = population_solution[p].color_num[color];
+            population_solution[p].psol[color][color_num] = i;
+            population_solution[p].index2[i] = color_num++;
+            population_solution[p].color_num[color] = color_num;
+        }
+    }
+
+    Population_solution temps;
+
+    int population_iteration = 0;
+    while (population.min_conflict != 0)
+    {
+        // random select two index from population as parents;
+        int p1 = rand() % num_population, p2;
+
+        do
+        {
+            p2 = rand() % num_population;
+        } while (p1 == p2);
+
+        memset(&temps, 0, sizeof(temps));
+
+        cross_over(p1, p2, temps.index1);
+
+        // reset adj_color_table and tabu_tenure_table to zero;
+        memset(adj_color_table, 0, sizeof(adj_color_table));
+        memset(tabu_tenure_table, 0, sizeof(tabu_tenure_table));
+        f = best_conflict = conflict_num =0;
+
+        tabu_search(temps.index1); // 仅仅需要对新形成的temps进行禁忌搜索;
+
+        for (i = 1; i <= num_vertex; i++)
+        {//变成划分的形式
+            int color = temps.index1[i];
+            int color_num = temps.color_num[color];
+            temps.psol[color][color_num] = i;
+            temps.index2[i] = color_num;
+            temps.color_num[color] = ++color_num;
+        }
+
+        int max_conflict = -1, max_conflict_index;
+
+        for (i = 0; i < num_population; i++)
+        {
+            if (population.num_conflict[i] > max_conflict)
+            {
+                max_conflict = population.num_conflict[i];
+                max_conflict_index = i;
+            }
+        }
+
+        population_solution[max_conflict_index] = temps; // 将种群中冲突数最大的替换成temps
+        population.num_conflict[max_conflict_index] = f;
+
+        if (f < population.min_conflict)
+        {
+            population.min_conflict = f;
+            population.min_conflict_index = max_conflict_index;
+        }
+
+        cout << "min conflict = " << population.min_conflict << endl;
+        cout << "min conflict index = " << population.min_conflict_index << endl;
+        population_iteration++;
+    }
+
+    double end_time = clock();
+    double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+    cout << "Population iteration: " << population_iteration << endl;
+    cout << "elapsed time(s): " << elapsed_time << endl;
+
+    if (population.min_conflict == 0)
+    {
+        fp = fopen("result.txt", "a+");
+        if (fp == nullptr)
+            printf("output file open error\n");
+        fprintf(fp, "%s %-9s %-15lf %-7d\n", argv[1], argv[2], elapsed_time , max_iter);
+
+        cout << "color of each vertex: ";
+        for(int i=1;i<=num_vertex;i++)
+        {
+            cout << population_solution[population.min_conflict_index].index1[i] << " ";
+        }
+        cout << endl;
+
+        cout << "conflict of solution 19: ";
+        cout << test.compute_conflict(population_solution[19].index1) << endl;
+
+        cout << "conflict of final solution: ";
+        cout << test.compute_conflict(population_solution[population.min_conflict_index].index1) << endl;
+    }
+    else
+        cout << "over time" << endl;
+
+    return 0;
 }
