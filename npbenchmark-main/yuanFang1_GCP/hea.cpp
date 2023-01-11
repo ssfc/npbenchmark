ca82d2dae35c0c1c441516805ce169f523d13e8b
YE GAO <sortsimilar@gmail.com>
2023-01-11 21:06:36 +0800

    change memset

--- npbenchmark-main/yuanFang1_GCP/hea.cpp
+++ npbenchmark-main/yuanFang1_GCP/hea.cpp
@@ -399,7 +399,7 @@ int main(int argc, char *argv[])
     for (p = 0; p < num_population; p++)
     {
         memset(test.adj_color_table, 0, sizeof(test.adj_color_table));
-        memset(tabu_tenure_table, 0, sizeof(tabu_tenure_table));
+        memset(tabu_tenure_table, 0, sizeof(int)*MaxPoint*MaxPoint);
         test.f = test.best_conflict = test.conflict_num = 0;
 
         // initialization: set random solution to each solution in the population;
