96632bb1a7b1d273857c1197c43ae9051483826f
YE GAO <sortsimilar@gmail.com>
2023-01-14 21:04:37 +0800

    add new file

---
+++ npbenchmark-main/tabucol_GCP/learn_gdb.cpp
@@ -0,0 +1,18 @@
+//
+// Created by take_ on 2023/1/14.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[4] = {1, 2, 3, 4};
+    for(int i=0;i<4;i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+
+    return 0;
+}
