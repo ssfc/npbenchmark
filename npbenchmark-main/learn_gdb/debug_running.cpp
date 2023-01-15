f85c2c722f5954f2b0c08194aba1d19e18b4443b
YE GAO <sortsimilar@gmail.com>
2023-01-16 07:48:00 +0800

    create new

---
+++ npbenchmark-main/learn_gdb/debug_running.cpp
@@ -0,0 +1,26 @@
+//
+// Created by take_ on 2023/1/14.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+void test(){}
+
+
+void test1()
+{
+    int i = 0;
+    i++;
+}
+
+
+int main()
+{
+    int *temp = NULL;
+    *temp = 10;
+
+    return 0;
+}
