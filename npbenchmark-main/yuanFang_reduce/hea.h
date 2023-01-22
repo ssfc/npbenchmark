5b724bd06c611bf4ec7ac565dbb1a48957caca67
YE GAO <sortsimilar@gmail.com>
2023-01-23 07:53:48 +0800

    add same files

---
+++ npbenchmark-main/yuanFang_reduce/hea.h
@@ -0,0 +1,106 @@
+//
+// Created by take_ on 2023/1/23.
+//
+
+#ifndef YUANFANG_REDUCE_HEA_H
+#define YUANFANG_REDUCE_HEA_H
+
+#include <algorithm>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+#define MaxPoint 1002
+#define MaxColor 300
+
+
+struct ArcNode {
+    int adj_vertex;
+    struct ArcNode *next;
+};
+
+struct VerNode {
+    ArcNode *first;
+};
+
+struct Move {
+    int u;
+    int vj;
+};
+
+class Population_solution {
+public:
+    int psol[MaxColor][MaxPoint];
+    int color_num[MaxColor];
+    int index1[MaxPoint];
+    int index2[MaxPoint];
+
+    Population_solution();
+    ~Population_solution();
+};
+
+class Population {
+public:
+    int min_conflict; // the min conflict among the population collection;
+    int min_conflict_index;
+    vector<int> num_conflict;
+
+    Population(int input_num_population);
+    ~Population();
+};
+
+// class HE;
+class Hybrid_Evolution
+{
+private:
+    int iter;
+
+public:
+    // variables;
+    int num_vertex;
+    int num_color;
+
+    vector<VerNode> adj_list;
+    vector<vector<int> > adj_color_table;
+    vector<vector<int> > tabu_tenure_table;
+
+    int best_conflict;
+    int f;
+    int conflict_num;
+    vector<int> conflicts;
+    vector<int> conflict_index;
+
+    int num_population;
+    int **solution_collection;
+    vector<Population_solution> population_solution;
+
+    int max_iter;
+
+
+    // functions
+    Hybrid_Evolution(int input_num_vertex, int input_num_color, int input_num_population);
+    ~Hybrid_Evolution();
+
+    void insert_adj_list(int i, int j);
+    void add_conflict(int adj_vertex);
+    void delete_conflict(int adj_vertex);
+
+    int tabu_search(int *solution);
+    Move find_move(const int *s);
+    void make_move(int u, int vj, int *solution);
+
+    void cross_over(int p1, int p2, int *index1) const;
+
+    // debug function: compute conflict of a solution
+    int compute_conflict(const int *solution);
+};
+
+
+#endif //YUANFANG1_GCP_HEA_H
