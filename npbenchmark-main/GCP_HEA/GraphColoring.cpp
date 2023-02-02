58ac48fa090979c48d17a0eacc5281398f38fd4d
YE GAO <sortsimilar@gmail.com>
2023-02-02 15:57:27 +0800

    add new

---
+++ npbenchmark-main/GCP_HEA/GraphColoring.cpp
@@ -0,0 +1,61 @@
+#include "GraphColoring.h"
+#include "tabucol.h"
+
+#include <fstream>
+#include <iostream>
+#include <random>
+
+
+using namespace std;
+
+namespace szx
+{
+    class Solver{
+        // random number generator.
+        mt19937 pseudoRandNumGen;
+        void initRand(int seed) { pseudoRandNumGen = mt19937(seed); }
+        int fastRand(int lb, int ub) { return (pseudoRandNumGen() % (ub - lb)) + lb; }
+        int fastRand(int ub) { return pseudoRandNumGen() % ub; }
+        int rand(int lb, int ub) { return uniform_int_distribution<int>(lb, ub - 1)(pseudoRandNumGen); }
+        int rand(int ub) { return uniform_int_distribution<int>(0, ub - 1)(pseudoRandNumGen); }
+
+    public:
+        void solve(NodeColors& output, GraphColoring& input, std::function<bool()> isTimeout, int seed) {
+            initRand(seed);
+
+            // TODO: implement your own solver which fills the `output` to replace the following trivial solver.
+            // sample solver: assign colors randomly (the solution can be infeasible).
+
+            //                      +----[ exit before timeout ]
+            //
+
+            /*
+            for (NodeId n = 0; !isTimeout() && (n < input.nodeNum); ++n)
+            {
+                output[n] = rand(input.colorNum);
+            }
+            */
+
+
+            Graph test_graph(input.nodeNum, input.edgeNum, input.colorNum, input.edges, seed);
+
+            // create graph;
+            // cerr << "Finish creating graph." << endl;
+            // test_graph.print_adj_list();
+
+            test_graph.tabu_search();
+
+            for (int i = 0;i < input.nodeNum;i++)
+            {
+                output[i] = test_graph.get_solution(i);
+            }
+
+
+        }
+    };
+
+    // solver.
+    void solveGraphColoring(NodeColors& output, GraphColoring& input, std::function<bool()> isTimeout, int seed) {
+        Solver().solve(output, input, isTimeout, seed);
+    }
+}
