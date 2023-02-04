02ce347b70460661a0a0e2d1d4cb92bb5110da5f
YE GAO <sortsimilar@gmail.com>
2023-02-04 22:07:19 +0800

    284	Modify adj_color_table

--- npbenchmark-main/debug_cross_over/hea.cpp
+++ npbenchmark-main/debug_cross_over/hea.cpp
@@ -88,7 +88,7 @@ Hybrid_Evolution::Hybrid_Evolution(int input_num_vertex, int input_num_color, in
     conflict = 0;
     best_conflict = 0;
 
-    adj_color_table.resize(input_num_vertex + 1);
+    adj_color_table.resize(input_num_vertex);
     for(auto & i : adj_color_table)
     {
         i.resize(input_num_color, 0);
@@ -148,13 +148,13 @@ void Hybrid_Evolution::find_move(vector<unsigned int> &solution)
     {
         unsigned int solution_i = solution[i]; // solution_i is color;
 
-        if (adj_color_table[i][solution_i] > 0) // if vertex i overlap its neighbor's color;
+        if (adj_color_table[i-1][solution_i] > 0) // if vertex i overlap its neighbor's color;
         {
             for (int j = 0; j < num_color; j++) // j is color;
             {
                 if (solution_i != j) // find a new color;
                 {//cpu流水线
-                    int this_delta = adj_color_table[i][j] - adj_color_table[i][solution_i]; // new-old, the less the better;
+                    int this_delta = adj_color_table[i-1][j] - adj_color_table[i-1][solution_i]; // new-old, the less the better;
                     if (tabu_tenure_table[i-1][j] <= iter) //nontabu move;
                     {
                         if (this_delta < min_delta)
@@ -248,8 +248,8 @@ void Hybrid_Evolution::make_move(vector<unsigned int> &solution)
     {
         int adj_list_node_moved_i = adj_list[moved.u][i];
 
-        adj_color_table[adj_list_node_moved_i][old_color]--;
-        adj_color_table[adj_list_node_moved_i][moved.vj]++;
+        adj_color_table[adj_list_node_moved_i-1][old_color]--;
+        adj_color_table[adj_list_node_moved_i-1][moved.vj]++;
     }
 }
 
@@ -269,7 +269,7 @@ void Hybrid_Evolution::tabu_search(vector<unsigned int> &solution, bool is_limit
             if (this_vertex_color == adj_color)
                 conflict++;
 
-            adj_color_table[i][adj_color]++; // initialize adjacent color table;
+            adj_color_table[i-1][adj_color]++; // initialize adjacent color table;
         }
     }
 
