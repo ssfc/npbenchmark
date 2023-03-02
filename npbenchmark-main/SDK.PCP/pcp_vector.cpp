de325a85e66f7dfc7ffb71fa79b5b57e08647a2b
YE GAO <sortsimilar@gmail.com>
2023-03-02 22:33:42 +0800

    Update pcp_vector.cpp

--- npbenchmark-main/SDK.PCP/pcp_vector.cpp
+++ npbenchmark-main/SDK.PCP/pcp_vector.cpp
@@ -510,9 +510,9 @@ void PCP_Vector::make_move(unsigned long long i, unsigned long long j)
             // Meaning: cancel penalty for deleting center l;
             // Comment: 由于|X 交 Cv| = 1, 所以这里面的循环只有一个数, 复杂度O(1).
             // Comment: 虽然中心l是当前中心集X中独一无二覆盖顶点v的, 但是由于swapped in的中心i也覆盖v, 所以它不再是不可或缺的了, 价值要减小. 这里减去的其实是LINE 14增加的量.
-            print_vector("center weights before", center_weights);
+            // print_vector("center weights before", center_weights);
             center_weights[intersect_center] = center_weights[intersect_center] - vertex_weights[v];
-            print_vector("center weights after", center_weights);
+            // print_vector("center weights after", center_weights);
         }
 
     }
