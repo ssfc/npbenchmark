a77834f77957f1aa7ea1eb13f09f0771654a5b6f
YE GAO <sortsimilar@gmail.com>
2023-03-04 20:50:00 +0800

    Update vwts.h

--- npbenchmark-main/wlz_pcp/vwts.h
+++ npbenchmark-main/wlz_pcp/vwts.h
@@ -17,21 +17,21 @@ class VWTS
 private:
     //图结构
     int num_vertex;
-    int num_center;//中心数量
-    int** center_coverages;//每个中心覆盖的节点
-    int* num_center_cover;//每个中心能够覆盖的节点数
+    int num_center; //中心数量
+    int** center_coverages; //每个中心覆盖的节点
+    int* num_center_cover; //每个中心能够覆盖的节点数
 
     //记录变量
-    bool* solution;//是否挑选为中心（求解）
-    int* center;//记录中心节点
-    int* covered_center_num;//每个顶点被多少个中心覆盖
-    int* covered_once;//仅被一个中心覆盖的顶点
-    int num_uncovered;//记录没有被覆盖的节点数，用作uncovered_list的下标
-    int* uncovered_list;//记录没有被覆盖的节点
-    int* vertex_weights;//每个节点权重，随着未覆盖时间增大逐渐增大
+    bool* solution; //是否挑选为中心（求解）
+    int* center; //记录中心节点
+    int* covered_center_num; //每个顶点被多少个中心覆盖
+    int* covered_once; //仅被一个中心覆盖的顶点
+    int num_uncovered; //没有被覆盖的节点数，用作uncovered_list的下标
+    int* uncovered_list; //记录没有被覆盖的节点
+    int* vertex_weights; //每个节点权重，随着未覆盖时间增大逐渐增大
     int f, best_f, best_delta_f; //f函数，记录加权和
-    int* delta;//记录中心i加入X或者删除对f的影响
-    int tabu_open, tabu_close;//禁忌期为1，记录禁忌元素
+    int* delta; //记录中心i加入X或者删除对f的影响
+    int tabu_open, tabu_close; //禁忌期为1，记录禁忌元素
 public:
     int tempiter;//暂时用于统计迭代次数
     int tempnum;//记录uncovered_num
