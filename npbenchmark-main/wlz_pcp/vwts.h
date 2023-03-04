a971620f5d562536324c3f23796f27a9c19baa68
YE GAO <sortsimilar@gmail.com>
2023-03-04 21:23:47 +0800

    Update vwts.h

--- npbenchmark-main/wlz_pcp/vwts.h
+++ npbenchmark-main/wlz_pcp/vwts.h
@@ -30,6 +30,7 @@ private:
     int* uncovered_vertices; //记录没有被覆盖的节点
     int* vertex_weights; //每个节点权重，随着未覆盖时间增大逐渐增大
     int f, best_f, best_delta_f; //f函数，记录加权和
+    
     int* delta; //记录中心i加入X或者删除对f的影响
     int tabu_open, tabu_close; //禁忌期为1，记录禁忌元素
 public:
