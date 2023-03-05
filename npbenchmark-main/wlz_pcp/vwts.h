c962a6e771c66468a057d7490b607ec33286f28b
YE GAO <sortsimilar@gmail.com>
2023-03-05 19:15:31 +0800

    Update vwts.h

--- npbenchmark-main/wlz_pcp/vwts.h
+++ npbenchmark-main/wlz_pcp/vwts.h
@@ -36,7 +36,7 @@ private:
     int* num_covered_center; //每个顶点被多少个中心覆盖
     int* covered_once; //仅被一个中心覆盖的顶点
     int* uncovered_vertices; //记录没有被覆盖的节点
-    int num_uncovered; //没有被覆盖的节点数，用作uncovered_list的下标
+    int num_uncovered; //没有被覆盖的节点数，用作uncovered_vertices的下标
 
     vector<int> vertex_weights; //每个节点权重，随着未覆盖时间增大逐渐增大
     vector<int> center_weights; //记录X加入中心i或者删除中心i对f(X)的影响
