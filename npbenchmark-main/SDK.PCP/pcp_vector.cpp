f4cbad9605d117662955889bc268ab12ca12251b
YE GAO <sortsimilar@gmail.com>
2023-03-08 08:51:33 +0800

    578	Move radius; move center_cover_vertex; move vertex_reach_center;

--- npbenchmark-main/SDK.PCP/pcp_vector.cpp
+++ npbenchmark-main/SDK.PCP/pcp_vector.cpp
@@ -10,17 +10,17 @@ PCP_Vector::PCP_Vector(int input_num_vertex, int input_num_center, int input_rad
                        vector<vector<int>> &input_coverages, int input_seed)
                        :num_vertex(input_num_vertex)
                        ,num_center(input_num_center)
+                       ,radius(input_radius)
+                       ,center_cover_vertex(input_num_vertex, dynamic_bitset<>(input_num_vertex))
+                       ,vertex_reach_center(input_num_vertex, dynamic_bitset<>(input_num_vertex))
                        ,solution(input_num_vertex)
                        ,uncovered_vertices(input_num_vertex)
                        ,moved{0, 0}
                        ,min_delta{INT_MAX}
 {
     init_rand(input_seed); // initialize random generator;
-    radius = input_radius;
 
-    center_cover_vertex.resize(input_num_vertex, dynamic_bitset<>(input_num_vertex));
     center_coverages.resize(input_num_vertex);
-    vertex_reach_center.resize(input_num_vertex, dynamic_bitset<>(input_num_vertex));
     vertex_reaching.resize(input_num_vertex);
     for(int i=0;i<input_coverages.size();i++) // i is center name
     {
