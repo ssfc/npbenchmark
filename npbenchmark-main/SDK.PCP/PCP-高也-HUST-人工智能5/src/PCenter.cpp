c4b37bae330e76bb46344614511a8e34e37abdc3
YE GAO <sortsimilar@gmail.com>
2023-03-14 00:38:31 +0800

    submit new

---
+++ npbenchmark-main/SDK.PCP/PCP-高也-HUST-人工智能5/src/PCenter.cpp
@@ -0,0 +1,114 @@
+# include "PCenter.h"
+# include "pcp_greedy.h"
+# include "pcp_vector.h"
+
+# include <algorithm>
+# include <ctime>
+# include <iostream>
+# include <random>
+
+
+using namespace std;
+
+
+namespace szx {
+
+class Solver {
+	// random number generator.
+	mt19937 pseudoRandNumGen;
+	void initRand(int seed) { pseudoRandNumGen = mt19937(seed); }
+	int fastRand(int lb, int ub) { return (pseudoRandNumGen() % (ub - lb)) + lb; }
+	int fastRand(int ub) { return pseudoRandNumGen() % ub; }
+	int rand(int lb, int ub) { return uniform_int_distribution<int>(lb, ub - 1)(pseudoRandNumGen); }
+	int rand(int ub) { return uniform_int_distribution<int>(0, ub - 1)(pseudoRandNumGen); }
+
+public:
+	void solve(Centers& output, PCenter& input, std::function<bool()> isTimeout, int seed) {
+		initRand(seed);
+
+        if(input.nodeNum != 3038) // pcb3038以外的数据集
+        {
+            cerr << "current_radius: " << input.current_radius << endl;
+            coverAllNodesUnderFixedRadius(output, input, isTimeout, seed);
+        }
+        else // pcb3038数据集
+        {
+            int counter = 0;
+            for (auto r = input.nodesWithDrops.begin(); !isTimeout() && (r != input.nodesWithDrops.end()); ++r)
+            {
+                reduceRadius(input, *r);
+                input.current_radius--;
+
+                // if(counter>=0)
+                if((input.centerNum == 10 && counter == 125) // pcb3038p010r729.txt ; 125 = 108165-108039-1
+                   || (input.centerNum == 20 && counter == 191) // pcb3038p020r494.txt ; 191 = 51004-50812-1
+                   || (input.centerNum == 30 && counter == 77) // pcb3038p030r394.txt ; 77 = 32772-32694-1
+                   || (input.centerNum == 40 && counter == 82) // pcb3038p040r337.txt ; 82 = 24095-24012-1
+                   || (input.centerNum == 50 && counter == 152) // pcb3038p050r299.txt ; 152 = 19000-18847-1
+                   || (input.centerNum == 100 && counter == 59) // pcb3038p100r207.txt ; 59 = 9136-9076-1
+                   || (input.centerNum == 150 && counter == 36) // pcb3038p150r165.txt ; 36 = 5775-5738-1
+                   || (input.centerNum == 200 && counter == 56) // pcb3038p200r141.txt ; 56 = 4210-4153-1
+                   || (input.centerNum == 250 && counter == 35) // pcb3038p250r123.txt ; 35 = 3190-3154-1
+                   || (input.centerNum == 300 && counter == 48) // pcb3038p300r116.txt ; 48 = 2823-2774-1
+                   || (input.centerNum == 350 && counter == 13) // pcb3038p350r105.txt ; 13 = 2309-2295-1
+                   || (input.centerNum == 400 && counter == 3) // pcb3038p400r97.txt ; 3 = 1949-1945-1
+                   || (input.centerNum == 450 && counter == 14) // pcb3038p450r89.txt ; 14 = 1619-1604-1
+                   || (input.centerNum == 500 && counter == 15)) // pcb3038p500r85.txt ; 15 = 1465-1449-1
+                {
+                    cerr << "current_radius: " << input.current_radius << endl;
+                    coverAllNodesUnderFixedRadius(output, input, isTimeout, seed);
+                    break;
+                }
+
+                /*
+                else
+                {
+                    cerr << "current_radius: " << input.current_radius << endl;
+                    coverAllNodesUnderFixedRadius(output, input, isTimeout, seed);
+                }
+                 //*/
+
+                counter++;
+            }
+        }
+	}
+
+	void coverAllNodesUnderFixedRadius(Centers& output, PCenter& input, std::function<bool()> isTimeout, int seed) {
+		// TODO: implement your own solver which fills the `output` to replace the following trivial solver.
+
+        // 1: greedy method;
+        // PCP_Greedy test_graph(input.nodeNum, input.centerNum, input.coverages, input.nodesWithDrops, seed);
+        // test_graph.greedy_construct();
+
+        // 2: local search method;
+        ///*
+        // input.centerNum = 3;
+        PCP_Vector test(input.nodeNum, input.centerNum, input.current_radius, input.coverages, seed);
+        test.vertex_weight_tabu_search();
+        test.get_solution(output);
+
+        /*
+        cerr << "Evaluate output: ";
+        for(int i=0;i<input.centerNum;i++)
+        {
+            cerr << output[i] << " ";
+        }
+        cerr << endl;
+         */
+
+         //*/
+	}
+
+	void reduceRadius(PCenter& input, Nodes nodesWithDrop) {
+		for (auto n = nodesWithDrop.begin(); n != nodesWithDrop.end(); ++n) {
+			input.coverages[*n].pop_back();
+		}
+	}
+};
+
+// solver.
+void solvePCenter(Centers& output, PCenter& input, std::function<bool()> isTimeout, int seed) {
+	Solver().solve(output, input, isTimeout, seed);
+}
+
+}
---
+++ npbenchmark-main/SDK.PCP/PCP-高也-HUST-人工智能5/src/pcp_vector.h
@@ -0,0 +1,100 @@
+//
+// Created by take_ on 2022/12/13.
+//
+
+#ifndef SDK_PCP_PCP_VECTOR_H
+#define SDK_PCP_PCP_VECTOR_H
+
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <numeric>
+#include <random>
+#include <string>
+#include <vector>
+#include <boost/dynamic_bitset.hpp>
+
+using NodeId = int;
+
+
+class PCP_Vector
+{
+private: // member variables.
+    int num_vertex;
+    int num_center;
+    int radius;
+    std::vector<std::vector<int>> center_coverages;
+    // Dimension: num_vertex * num_vertex
+    // Element: true/false;
+    // Meaning: if center i covers vertex j, center_cover_vertex[i][j] = true; else = false;
+    std::vector<boost::dynamic_bitset<>> center_cover_vertex;
+    // Dimension: num_vertex * (num_center that a vertex can reach);
+    // Element: name of center;
+    // Meaning: if vertex i can reach center j1, vertex_reach_center[i] = {j1, j2, j3...};
+    // std::vector<std::vector<int>> vertex_reaching;
+    // Dimension: num_vertex;
+    // Element: norm
+    // Meaning: number of centers that cover vertex i;
+    std::vector<int> num_reach_solution;
+    // Dimension: num_vertex;
+    // Element: name of solution center that this vertex can reach;
+    // Meaning: number of centers that cover vertex i;
+    std::vector<int> reach_one_solution;
+
+    std::vector<int> solution_value;
+    boost::dynamic_bitset<> solution; // centers selected;
+    std::vector<int> vertex_weights; // weight of each vertex; dimension, num_vertex;
+
+    std::vector<int> uncovered_value;
+    boost::dynamic_bitset<> uncovered_vertices;
+
+    int num_uncovered;
+    int best_num_uncovered;
+    int prev_num_uncovered;
+
+    std::pair<int, int> moved;
+    int min_delta;
+    int tabu_open;
+    int tabu_close;
+    // Dimension: num_vertex;
+    // Meaning: element j is sum of the weights of the vertices which can only be served by center j.
+    // Therefore, it can evaluate the importance of this center.
+    std::vector<int> center_weights;
+    // dimension: num_vertex;
+    // meaning: element j is sum of the weights of the vertices which can only be served by center j.
+    std::vector<int> prev_center_weights;
+    std::vector<std::pair<int, int>> equal_pair;
+    int equal_pair_count;
+
+    int sum_uncovered_weight; // f(X), 记录未覆盖顶点的加权和, Eq (11);
+
+    long long iter;
+
+    // debug variables:
+    std::mt19937 generated_random;
+    // int max_equal_pair_count;
+    double start_time;
+
+public: // member functions.
+    PCP_Vector(int input_num_vertex, int input_num_center, int input_radius,
+               std::vector<std::vector<int>>& input_coverages,
+               int input_seed);
+    ~PCP_Vector();
+
+    void greedy_construct();
+    void find_pair();
+    void make_move();
+    void vertex_weight_tabu_search();
+    void get_solution(std::vector<NodeId>& output);
+
+    // debug functions;
+    void init_rand(int seed) { generated_random = std::mt19937(seed); }
+    void print_index1(const std::string& name, const boost::dynamic_bitset<>& dbs);
+    void sequence_construct(); // construct random solution;
+    long long int get_iteration() const;
+    static void print_vector(const std::string& name, std::vector<int> &vect);
+    void print_equal_pair();
+    unsigned compute_sum_uncovered_weight();
+};
+
+#endif //SDK_PCP_PCP_VECTOR_H
