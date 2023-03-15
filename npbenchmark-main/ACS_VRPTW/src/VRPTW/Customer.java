d075f23c80bc1a9b9e492dab982b14b93586fa96
YE GAO <sortsimilar@gmail.com>
2023-03-15 16:32:55 +0800

    Update Customer.java

--- npbenchmark-main/ACS_VRPTW/src/VRPTW/Customer.java
+++ npbenchmark-main/ACS_VRPTW/src/VRPTW/Customer.java
@@ -1,13 +1,15 @@
 package VRPTW;
 
-public class Customer {
+public class Customer
+{
     int Number;//节点自身编号
     int R;//节点所属车辆路径编号
     int X, Y;//节点横纵坐标
     int Begin, End, Service;//节点被访问的最早时间，最晚时间以及服务时长
     int Demand;//节点的需求容量
 
-    public Customer() {
+    public Customer()
+    {
         Number = 0;
         R = 0;
         Begin = 0;
@@ -18,7 +20,8 @@ public class Customer {
         Demand = 0;
     }
 
-    public Customer copy() {
+    public Customer copy()
+    {
         Customer newCustomer = new Customer();
         newCustomer.Number = Number;
         newCustomer.R = R;
