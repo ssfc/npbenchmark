a3d292cc6dcbc286684d82066f99dc30d1c3eb2d
YE GAO <sortsimilar@gmail.com>
2023-03-15 19:23:27 +0800

    Update Route.java

--- npbenchmark-main/ACS_VRPTW/src/VRPTW/Route.java
+++ npbenchmark-main/ACS_VRPTW/src/VRPTW/Route.java
@@ -2,7 +2,8 @@ package VRPTW;
 
 import java.util.ArrayList;
 
-public class Route {
+public class Route
+{
     public double load;//데係쨌쓺넓潼좆
     public double distance;//데係쨌쓺悧낀똑
     public double time;
