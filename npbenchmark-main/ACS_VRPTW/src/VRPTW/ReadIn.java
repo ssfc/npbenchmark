16bd797fe5d49dc1b8aba26a83582e0fd0495e3e
YE GAO <sortsimilar@gmail.com>
2023-03-15 19:19:36 +0800

    Update ReadIn.java

--- npbenchmark-main/ACS_VRPTW/src/VRPTW/ReadIn.java
+++ npbenchmark-main/ACS_VRPTW/src/VRPTW/ReadIn.java
@@ -5,20 +5,24 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 
-public class ReadIn {
+public class ReadIn
+{
     public int customerNr; // 客户数量
     public int capacity; // 车辆容量
     public double[][] Graph; // 记录图
     public Customer[] customers; // 存储客户数据
 
     //计算图上各节点间的距离
-    private double Distance ( Customer C1, Customer C2 ) {
+    private double Distance ( Customer C1, Customer C2 )
+    {
         return sqrt ( ( C1.X - C2.X ) * ( C1.X - C2.X ) + ( C1.Y - C2.Y ) * ( C1.Y - C2.Y ) );
     }
 
     //读取数据
-    public void Read(String fileName){
-        try {
+    public void Read(String fileName)
+    {
+        try
+        {
             Scanner in = new Scanner(new FileReader(fileName));
 
             in.next();
@@ -27,11 +31,13 @@ public class ReadIn {
 
             customers = new Customer[customerNr + 10];
             Graph = new double[customerNr + 10][customerNr + 10];
-            for(int i = 0; i < customerNr + 10; i++) {
+            for(int i = 0; i < customerNr + 10; i++)
+            {
                 customers[i] = new Customer();
             }
 
-            for ( int i = 0; i <= customerNr; ++i ) {
+            for ( int i = 0; i <= customerNr; ++i )
+            {
                 customers[i].Number=in.nextInt();
                 customers[i].X=in.nextInt();
                 customers[i].Y=in.nextInt();
@@ -43,7 +49,9 @@ public class ReadIn {
             }
 
             in.close();
-        }catch (FileNotFoundException e) {
+        }
+        catch (FileNotFoundException e)
+        {
             // 未找到文件
             System.out.println("File not found!");
             System.exit(-1);
