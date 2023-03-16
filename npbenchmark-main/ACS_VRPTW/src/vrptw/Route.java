d79a7809f0709d58c4ea8990df5e964e82c764d4
YE GAO <sortsimilar@gmail.com>
2023-03-16 21:59:07 +0800

    remove public

--- npbenchmark-main/ACS_VRPTW/src/vrptw/Route.java
+++ npbenchmark-main/ACS_VRPTW/src/vrptw/Route.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class Route
 {
-    public double load;//데係쨌쓺넓潼좆
-    public double distance;//데係쨌쓺悧낀똑
-    public double time;
-    public ArrayList <Integer> customers = new ArrayList<>();
+    double load;//데係쨌쓺넓潼좆
+    double distance;//데係쨌쓺悧낀똑
+    double time;
+    ArrayList <Integer> customers = new ArrayList<>();
 }
