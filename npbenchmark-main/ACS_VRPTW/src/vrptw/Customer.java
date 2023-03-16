fc2263d65739ce3feea6ae4eb98a903036b18e71
YE GAO <sortsimilar@gmail.com>
2023-03-16 20:31:01 +0800

    rename R as route_number

--- npbenchmark-main/ACS_VRPTW/src/vrptw/Customer.java
+++ npbenchmark-main/ACS_VRPTW/src/vrptw/Customer.java
@@ -4,7 +4,7 @@ public class Customer
 {
     // member variables;
     int node_number;//节点自身编号
-    int R;//节点所属车辆路径编号
+    int route_number;//节点所属车辆路径编号
     int x; //节点横坐标
     // chatGPT: 变量横坐标通常命名为x或者X，具体取决于编程语言的命名规范和开发者的个人喜好。在Java中，一般使用小写字母x来表示变量横坐标值。
     int y; //节点纵坐标
@@ -18,7 +18,7 @@ public class Customer
     public Customer()
     {
         node_number = 0;
-        R = 0;
+        route_number = 0;
         ready_time = 0;
         due_time = 0;
         service_time = 0;
@@ -31,7 +31,7 @@ public class Customer
     {
         Customer newCustomer = new Customer();
         newCustomer.node_number = node_number;
-        newCustomer.R = R;
+        newCustomer.route_number = route_number;
         newCustomer.ready_time = ready_time;
         newCustomer.due_time = due_time;
         newCustomer.service_time = service_time;
