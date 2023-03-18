6710f2d46179b9bcb1a56a54b3c4ab42fc5f99fe
YE GAO <sortsimilar@gmail.com>
2023-03-18 22:20:48 +0800

    {}

--- npbenchmark-main/ACS_VRPTW/src/vrptw/AntColonySystem.java
+++ npbenchmark-main/ACS_VRPTW/src/vrptw/AntColonySystem.java
@@ -51,7 +51,9 @@ public class AntColonySystem
         solutions = new Solution[num_agents + 1]; // 设置初始agents数量和城市数一样多, 后面会慢慢减小;
         untreated = new ArrayList[num_agents + 1]; // 数组数量等于agents数
         for (int i = 0; i < num_agents + 1; i++)
+        {
             untreated[i] = new ArrayList<>();
+        }
         agent_position = new int[num_agents + 1];
         pheromone = new double[num_customers + 1][num_customers + 1];
         herustic = new double[num_customers + 1][num_customers + 1];
