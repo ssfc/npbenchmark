8c5339d4fca35e5668ab97ad1b8672a4bb9df6b6
YE GAO <sortsimilar@gmail.com>
2023-03-18 22:12:44 +0800

    Replace num_customers with num_nodes in ReadIn

--- npbenchmark-main/ACS_VRPTW/src/vrptw/ReadIn.java
+++ npbenchmark-main/ACS_VRPTW/src/vrptw/ReadIn.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class ReadIn
 {
-    int num_customers; // 客户数量
+    int num_nodes; // 客户数量+1
     int max_num_agents; // 最大可用车辆
     int capacity; // 车辆容量
     double[][] graph; // 记录图
@@ -26,18 +26,18 @@ public class ReadIn
         {
             Scanner in = new Scanner(new FileReader(fileName));
 
-            num_customers = in.nextInt();
+            num_nodes = in.nextInt();
             max_num_agents = in.nextInt();
             capacity = in.nextInt();
 
-            customers = new Customer[num_customers];
-            graph = new double[num_customers][num_customers];
-            for(int i = 0; i < num_customers; i++)
+            customers = new Customer[num_nodes];
+            graph = new double[num_nodes][num_nodes];
+            for(int i = 0; i < num_nodes; i++)
             {
                 customers[i] = new Customer();
             }
 
-            for ( int i = 0; i < num_customers; i++ ) // 之所以+1, 是因为编号从0开始, 仓库也算作一行;
+            for ( int i = 0; i < num_nodes; i++ ) // 之所以+1, 是因为编号从0开始, 仓库也算作一行;
             {
                 customers[i].x = in.nextInt();
                 customers[i].y = in.nextInt();
@@ -57,9 +57,9 @@ public class ReadIn
             System.exit(-1);
         }
 
-        for ( int i = 0; i < num_customers; i++ ) // 之所以+1, 是因为编号从0开始, 仓库也算作一行;
+        for ( int i = 0; i < num_nodes; i++ ) // 之所以+1, 是因为编号从0开始, 仓库也算作一行;
         {
-            for (int j = 0; j < num_customers; j++) // 之所以+1, 是因为编号从0开始, 仓库也算作一行;
+            for (int j = 0; j < num_nodes; j++) // 之所以+1, 是因为编号从0开始, 仓库也算作一行;
             {
                 graph[i][j] = compute_distance(customers[i], customers[j]);
             }
