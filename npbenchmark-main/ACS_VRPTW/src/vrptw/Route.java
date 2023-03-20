29459ea996fbdee1e33c997fa4439d4a27ae99c2
YE GAO <sortsimilar@gmail.com>
2023-03-20 16:24:39 +0800

    replace customers in class Route with nodes
    comment: 因为包含仓库, 这样命名更合适一些

--- npbenchmark-main/ACS_VRPTW/src/vrptw/Route.java
+++ npbenchmark-main/ACS_VRPTW/src/vrptw/Route.java
@@ -7,5 +7,5 @@ public class Route
     double load;//데係쨌쓺넓潼좆
     double distance;//데係쨌쓺悧낀똑
     double time;
-    ArrayList <Integer> customers = new ArrayList<>();
+    ArrayList <Integer> nodes = new ArrayList<>();
 }
