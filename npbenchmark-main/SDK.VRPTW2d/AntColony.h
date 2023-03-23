358a27b0837caf02d2604025b51e14c4ce30fea2
YE GAO <sortsimilar@gmail.com>
2023-03-23 15:28:10 +0800

    add nodes to cpp
    add nodes to cpp; 算了懒得让它们相互引用了, 直接把struct Node2d复制过来算逑; done

--- npbenchmark-main/SDK.VRPTW2d/AntColony.h
+++ npbenchmark-main/SDK.VRPTW2d/AntColony.h
@@ -4,9 +4,24 @@
 
 #ifndef SDK_VRPTW2D_ANTCOLONY_H
 #define SDK_VRPTW2D_ANTCOLONY_H
-
+#include <array>
 #include <random>
+#include <vector>
 
+using NodeId = int;
+using VehicleId = int;
+using Coord = double;
+using Capacity = int;
+using Time = int;
+using Coord2d = std::array<Coord, 2>;
+// this struct corresponds to Node.java;
+struct Node2d {
+    Coord2d coords; // double
+    Capacity demand; // int
+    Time minStayTime; // int
+    Time windowBegin; // int
+    Time windowEnd; // int
+};
 
 class AntColony
 {
@@ -15,6 +30,10 @@ private: // member variables.
     int num_agents; // agent数量
     int max_num_agents; // 最大agents数量;
     int capacity; // 车辆容量
+
+    // Dimension: num__nodes == 客户数量+1
+    // Meaning: 存储客户数据, 就是算例中一行一行的东西;
+    std::vector<Node2d> nodes;
 
     // debug variables:
     std::mt19937 generated_random;
