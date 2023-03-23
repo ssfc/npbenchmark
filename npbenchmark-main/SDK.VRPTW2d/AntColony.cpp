c58a29651398cfd4ea799540be736a36a9f630eb
YE GAO <sortsimilar@gmail.com>
2023-03-23 10:36:31 +0800

    529	Add num_nodes and num_agents

--- npbenchmark-main/SDK.VRPTW2d/AntColony.cpp
+++ npbenchmark-main/SDK.VRPTW2d/AntColony.cpp
@@ -1,3 +1,19 @@
 //
 // Created by take_ on 2023/3/22.
 //
+
+# include "AntColony.h"
+
+using namespace std;
+
+
+AntColony::AntColony(int input_num_nodes, int input_num_agents,
+                     int input_seed)
+{
+    init_rand(input_seed); // initialize random generator;
+
+
+}
+
+AntColony::~AntColony()
+= default;
--- npbenchmark-main/SDK.VRPTW2d/AntColony.h
+++ npbenchmark-main/SDK.VRPTW2d/AntColony.h
@@ -5,8 +5,26 @@
 #ifndef SDK_VRPTW2D_ANTCOLONY_H
 #define SDK_VRPTW2D_ANTCOLONY_H
 
+#include <random>
 
 
+class AntColony
+{
+private: // member variables.
+    int num_nodes; // 客户数量+1, 因为算上了仓库; 101
+    int num_agents; // agent数量
+
+    // debug variables:
+    std::mt19937 generated_random;
+
+public: // member functions.
+    AntColony(int input_num_nodes, int input_num_agents,
+               int input_seed);
+    ~AntColony();
+
+    // debug functions;
+    void init_rand(int seed) { generated_random = std::mt19937(seed); }
+};
 
 
 
