e52119f30eaa840f7525cd00e9835a7e99b20e48
YE GAO <sortsimilar@gmail.com>
2023-03-23 18:19:29 +0800

    540	Let *10 in compute_distance and let three time times 10 in java

--- npbenchmark-main/ACS_VRPTW/src/vrptw/ReadIn.java
+++ npbenchmark-main/ACS_VRPTW/src/vrptw/ReadIn.java
@@ -22,7 +22,7 @@ public class ReadIn
     //计算图上各节点间的距离
     private double compute_distance(Node c1, Node c2)
     {
-        return ((int) 10 * sqrt((c1.x - c2.x) * (c1.x - c2.x) + (c1.y - c2.y) * (c1.y - c2.y)) ) / 10;
+        return ((int) 10 * sqrt((c1.x - c2.x) * (c1.x - c2.x) + (c1.y - c2.y) * (c1.y - c2.y)) ) ;
     }
 
     //读取数据
@@ -48,9 +48,9 @@ public class ReadIn
                 nodes[i].x = in.nextInt();
                 nodes[i].y = in.nextInt();
                 nodes[i].demand = in.nextInt();
-                nodes[i].service_time = in.nextInt();
-                nodes[i].ready_time = in.nextInt();
-                nodes[i].due_time = in.nextInt();
+                nodes[i].service_time = in.nextInt() * 10;
+                nodes[i].ready_time = in.nextInt() * 10;
+                nodes[i].due_time = in.nextInt() * 10;
                 //System.out.println("customerNr X,Y = " + customers[i].X + "\t" + customers[i].Y);
             }
 
