f8f20c89aacaa262b1d6f0390a9745d917036ca5
YE GAO <sortsimilar@gmail.com>
2023-03-31 13:13:46 +0800

    comment

--- npbenchmark-main/SDK.VRPTW2d/Main.cpp
+++ npbenchmark-main/SDK.VRPTW2d/Main.cpp
@@ -11,10 +11,12 @@ using namespace szx;
 
 void loadInput(istream& is, VRPTW2d& vrp)
 {
+    // 使用了istream的>>操作符，将输入流中的前三个数分别赋值给vrp对象的nodeNum、maxVehicleNum和vehicleCapacity成员变量。
 	is >> vrp.nodeNum >> vrp.maxVehicleNum >> vrp.vehicleCapacity;
 	vrp.nodes.resize(vrp.nodeNum);
 	for (auto & node : vrp.nodes)
     {
+        // 使用了>>操作符，将输入流中的节点坐标、需求量、最小停留时间、时间窗口开始时间和结束时间分别赋值给了节点的对应成员变量。
 		is >> node.coords[0] >> node.coords[1] >> node.demand >> node.min_stay_time >> node.window_begin >> node.window_end;
 	}
 }
