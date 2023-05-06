06f46b0f8249e3823f63a8c9090b6e3045b28fc6
YE GAO <sortsimilar@gmail.com>
2023-05-06 18:02:58 +0800

    add new

--- npbenchmark-main/travel_thief/tppcpp/main.cpp
+++ npbenchmark-main/travel_thief/tppcpp/main.cpp
@@ -1,10 +1,71 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 
 using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string filename = "../data/a280_n279_bounded-strongly-corr_01.ttp";
+    ifstream file(filename);
+
+    if (!file.is_open()) {
+        cerr << "Failed to open file " << filename << endl;
+        return 1;
+    }
+
+    string line;
+    while (getline(file, line)) {
+        // Remove leading and trailing whitespace
+        line.erase(line.find_last_not_of(" \t\r\n") + 1);
+        line.erase(0, line.find_first_not_of(" \t\r\n"));
+
+        // Skip comments and empty lines
+        if (line.empty() || line[0] == '#') {
+            continue;
+        }
+
+        // Parse the line
+        string key, value;
+        istringstream iss(line);
+        iss >> key >> value;
+
+        if (key == "PROBLEM") {
+            cout << "Problem name: " << value << endl;
+        } else if (key == "KNAPSACK") {
+            cout << "Knapsack data type: " << value << endl;
+        } else if (key == "DIMENSION:") {
+            int dimension = stoi(value);
+            cout << "Dimension: " << dimension << endl;
+        } else if (key == "NUMBER") {
+            int num_items = stoi(value);
+            cout << "Number of items: " << num_items << endl;
+        } else if (key == "CAPACITY") {
+            int capacity = stoi(value);
+            cout << "Capacity of knapsack: " << capacity << endl;
+        } else if (key == "MIN") {
+            float min_speed = stof(value);
+            cout << "Min speed: " << min_speed << endl;
+        } else if (key == "MAX") {
+            float max_speed = stof(value);
+            cout << "Max speed: " << max_speed << endl;
+        } else if (key == "RENTING") {
+            float renting_ratio = stof(value);
+            cout << "Renting ratio: " << renting_ratio << endl;
+        } else if (key == "EDGE_WEIGHT_TYPE:") {
+            cout << "Edge weight type: " << value << endl;
+        } else if (key == "NODE_COORD_SECTION") {
+            // Read the node coordinates section
+            cout << "Node coordinates:" << endl;
+
+            int index;
+            float x, y;
+            while (file >> index >> x >> y) {
+                cout << index << " (" << x << ", " << y << ")" << endl;
+            }
+        }
+    }
+
+    file.close();
     return 0;
 }
