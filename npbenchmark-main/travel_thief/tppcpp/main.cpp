38555c93f45d456a9c34d4ca4f82f8a9099c4ce1
YE GAO <sortsimilar@gmail.com>
2023-05-08 18:38:12 +0800

    move in value

--- npbenchmark-main/travel_thief/tppcpp/main.cpp
+++ npbenchmark-main/travel_thief/tppcpp/main.cpp
@@ -44,52 +44,62 @@ int main()
         // Parse the line
         string key, value;
         istringstream iss(line);
-        iss >> key >> value;
+        iss >> key;
 
         if(key == "problem_name:")
         {
+            iss >> value;
             cout << "Problem name: " << value << endl;
         }
         else if(key == "knapsack_data_type:")
         {
+            iss >> value;
             cout << "Knapsack data type: " << value << endl;
         }
         else if(key == "DIMENSION:")
         {
+            iss >> value;
             int dimension = stoi(value);
             cout << "Dimension: " << dimension << endl;
         }
         else if(key == "number_of_items:")
         {
+            iss >> value;
             int num_items = stoi(value);
             cout << "Number of items: " << num_items << endl;
         }
         else if(key == "capacity_of_knapsack:")
         {
+            iss >> value;
             int capacity = stoi(value);
             cout << "Capacity of knapsack: " << capacity << endl;
         }
         else if(key == "min_speed:")
         {
+            iss >> value;
             float min_speed = stof(value);
             cout << "Min speed: " << min_speed << endl;
         }
         else if(key == "max_speed:")
         {
+            iss >> value;
             float max_speed = stof(value);
             cout << "Max speed: " << max_speed << endl;
         }
         else if(key == "renting_ratio:")
         {
+            iss >> value;
             float renting_ratio = stof(value);
             cout << "Renting ratio: " << renting_ratio << endl;
         }
         else if(key == "EDGE_WEIGHT_TYPE:")
         {
+            iss >> value;
             cout << "Edge weight type: " << value << endl;
         }
         else if(key == "NODE_COORD_SECTION")
         {
+            iss >> value;
             // Read the node coordinates section
             cout << "Node coordinates:" << value << endl;
 
@@ -102,6 +112,7 @@ int main()
         }
         else if(key == "ITEMS")
         {
+            iss >> value;
             // Read the node coordinates section
             cout << "Items section:" << endl;
 
