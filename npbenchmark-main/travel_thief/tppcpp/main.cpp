bcd3221597144fdf02022d9b4c3eeeb3a782a1d1
YE GAO <sortsimilar@gmail.com>
2023-05-08 15:12:25 +0800

    Update main.cpp

--- npbenchmark-main/travel_thief/tppcpp/main.cpp
+++ npbenchmark-main/travel_thief/tppcpp/main.cpp
@@ -11,14 +11,14 @@ int main()
     string filename = "../data/a280_n279_bounded-strongly-corr_01.ttp";
     ifstream file(filename);
 
-    if (!file.is_open())
+    if(!file.is_open())
     {
         cerr << "Failed to open file " << filename << endl;
         return 1;
     }
 
     string line;
-    while (getline(file, line))
+    while(getline(file, line))
     {
         // Remove leading and trailing whitespace
         line.erase(line.find_last_not_of(" \t\r\n") + 1);
@@ -31,7 +31,7 @@ int main()
         // cout << line << endl;
 
         // Skip comments and empty lines
-        if (line.empty() || line[0] == '#')
+        if(line.empty() || line[0] == '#')
         {
             continue;
         }
@@ -41,49 +41,49 @@ int main()
         istringstream iss(line);
         iss >> key >> value;
 
-        if (key == "problem_name:")
+        if(key == "problem_name:")
         {
             cout << "Problem name: " << value << endl;
         }
-        else if (key == "knapsack_data_type:")
+        else if(key == "knapsack_data_type:")
         {
             cout << "Knapsack data type: " << value << endl;
         }
-        else if (key == "DIMENSION:")
+        else if(key == "DIMENSION:")
         {
             int dimension = stoi(value);
             cout << "Dimension: " << dimension << endl;
         }
-        else if (key == "number_of_items:")
+        else if(key == "number_of_items:")
         {
             int num_items = stoi(value);
             cout << "Number of items: " << num_items << endl;
         }
-        else if (key == "CAPACITY")
+        else if(key == "CAPACITY")
         {
             int capacity = stoi(value);
             cout << "Capacity of knapsack: " << capacity << endl;
         }
-        else if (key == "MIN")
+        else if(key == "MIN")
         {
             float min_speed = stof(value);
             cout << "Min speed: " << min_speed << endl;
         }
-        else if (key == "MAX")
+        else if(key == "MAX")
         {
             float max_speed = stof(value);
             cout << "Max speed: " << max_speed << endl;
         }
-        else if (key == "RENTING")
+        else if(key == "RENTING")
         {
             float renting_ratio = stof(value);
             cout << "Renting ratio: " << renting_ratio << endl;
         }
-        else if (key == "EDGE_WEIGHT_TYPE:")
+        else if(key == "EDGE_WEIGHT_TYPE:")
         {
             cout << "Edge weight type: " << value << endl;
         }
-        else if (key == "NODE_COORD_SECTION")
+        else if(key == "NODE_COORD_SECTION")
         {
             // Read the node coordinates section
             cout << "Node coordinates:" << endl;
