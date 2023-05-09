d39c688e183d23586ace204014b681fc74aac4bd
YE GAO <sortsimilar@gmail.com>
2023-05-09 09:11:01 +0800

    update

--- npbenchmark-main/travel_thief/tppcpp/main.cpp
+++ npbenchmark-main/travel_thief/tppcpp/main.cpp
@@ -18,89 +18,54 @@ int main()
     }
 
     string line;
-    while(getline(file, line))
+    while (getline(file, line))
     {
-        // Remove leading and trailing whitespace
-        line.erase(line.find_last_not_of(" \t\r\n") + 1);
-        line.erase(0, line.find_first_not_of(" \t\r\n"));
-        line = regex_replace(line, regex("bounded strongly corr"), "bounded-strongly-corr");
-        // cout << line << endl;
-
-        // Skip comments and empty lines
-        if(line.empty() || line[0] == '#')
+        if (line.find("PROBLEM NAME:") != string::npos)
         {
-            continue;
+            line.erase(0, 14); // 删除"PROBLEM NAME: "，保留问题名称
+            cout << "PROBLEM NAME:" << line << endl;
         }
-
-        // Parse the line
-        string word_1, word_2, word_3, word_4, word_5, word_6;
-        istringstream iss(line);
-        iss >> word_1;
-
-        if(word_1 == "PROBLEM")
+        else if (line.find("KNAPSACK DATA TYPE:") != string::npos)
         {
-            iss >> word_2 >> word_3;
-            cout << "PROBLEM NAME: " << word_3 << endl;
+            line.erase(0, 20); // 删除"KNAPSACK DATA TYPE: "，保留背包数据类型
+            cout << "KNAPSACK DATA TYPE:" << line << endl;
         }
-        else if(word_1 == "KNAPSACK")
+        else if (line.find("DIMENSION:") != string::npos)
         {
-            iss >> word_2 >> word_3 >> word_4;
-            cout << "KNAPSACK DATA TYPE: " << word_4 << endl;
+            line.erase(0, 11); // 删除"DIMENSION: "，保留维度
+            cout << "维度：" << line << endl;
         }
-        else if(word_1 == "DIMENSION:")
+        else if (line.find("NUMBER OF ITEMS:") != string::npos)
         {
-            iss >> word_2;
-            int dimension = stoi(word_2);
-            cout << "DIMENSION: " << dimension << endl;
-        }
-        else if(word_1 == "NUMBER")
-        {
-            iss >> word_2 >> word_3 >> word_4;
-            int num_items = stoi(word_4);
-            cout << "NUMBER OF ITEMS: " << num_items << endl;
-        }
-        else if(word_1 == "CAPACITY")
-        {
-            iss >> word_2 >> word_3 >> word_4;
-            int capacity = stoi(word_4);
-            cout << "CAPACITY OF KNAPSACK: " << capacity << endl;
-        }
-        else if(word_1 == "MIN")
-        {
-            iss >> word_2 >> word_3;
-            float min_speed = stof(word_3);
-            cout << "MIN SPEED: " << min_speed << endl;
-        }
-        else if(word_1 == "MAX")
-        {
-            iss >> word_2 >> word_3;
-            float max_speed = stof(word_3);
-            cout << "MAX SPEED: " << max_speed << endl;
-        }
-        else if(word_1 == "RENTING")
-        {
-            iss >> word_2 >> word_3;
-            float renting_ratio = stof(word_3);
-            cout << "RENTING RATIO: " << renting_ratio << endl;
-        }
-        else if(word_1 == "EDGE_WEIGHT_TYPE:")
-        {
-            iss >> word_2;
-            cout << "Edge weight type: " << word_2 << endl;
-        }
-        else if(word_1 == "NODE_COORD_SECTION")
-        {
-            iss >> word_2 >> word_3 >> word_4;
-            // Read the node coordinates section
-            cout << "Node coordinates:" << word_2 << word_3 << word_4 << endl;
-
-            int index;
-            float x, y;
-            while (file >> index >> x >> y)
-            {
-                cout << index << " (" << x << ", " << y << ")" << endl;
+            line.erase(0, 17); // 删除"NUMBER OF ITEMS: "，保留物品数量
+            cout << "物品数量：" << line << endl;
+        } else if (line.find("CAPACITY OF KNAPSACK:") != string::npos) {
+            line.erase(0, 22); // 删除"CAPACITY OF KNAPSACK: "，保留背包容量
+            cout << "背包容量：" << line << endl;
+        } else if (line.find("MIN SPEED:") != string::npos) {
+            line.erase(0, 11); // 删除"MIN SPEED: "，保留最小速度
+            cout << "最小速度：" << line << endl;
+        } else if (line.find("MAX SPEED:") != string::npos) {
+            line.erase(0, 11); // 删除"MAX SPEED: "，保留最大速度
+            cout << "最大速度：" << line << endl;
+        } else if (line.find("RENTING RATIO:") != string::npos) {
+            line.erase(0, 14); // 删除"RENTING RATIO: "，保留租金比率
+            cout << "租金比率：" << line << endl;
+        } else if (line.find("EDGE_WEIGHT_TYPE:") != string::npos) {
+            line.erase(0, 18); // 删除"EDGE_WEIGHT_TYPE: "，保留边权类型
+            cout << "边权类型：" << line << endl;
+        } else if (line == "NODE_COORD_SECTION  (INDEX, X, Y):") {
+            cout << "节点坐标：" << endl;
+            while (getline(file, line)) {
+                if (line.empty()) {
+                    break; // 节点坐标结束
+                }
+                int index, x, y;
+                sscanf(line.c_str(), "%d %d %d", &index, &x, &y); // 从字符串中解析出节点索引、横坐标、纵坐标
+                cout << index << "\t" << x << "\t" << y << endl; // 输出节点坐标
             }
-        }
+        } else if (line == "ITEMS SECTION  (INDEX, PROFIT, WEIGHT, ASSIGNED NODE NUMBER):") {
+            }
 
     }
 
