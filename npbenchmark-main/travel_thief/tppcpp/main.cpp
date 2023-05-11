18ebdab2941df0b95f8b73a4df9617821778c1ea
YE GAO <sortsimilar@gmail.com>
2023-05-11 15:58:43 +0800

    Update main.cpp

--- npbenchmark-main/travel_thief/tppcpp/main.cpp
+++ npbenchmark-main/travel_thief/tppcpp/main.cpp
@@ -29,61 +29,61 @@ int main()
         {
             line.erase(0, 14); // 删除"PROBLEM NAME: "，保留问题名称
             test.set_problem_name(line);
-            cout << "PROBLEM NAME:" << test.get_problem_name() << endl;
+            cerr << "PROBLEM NAME:" << test.get_problem_name() << endl;
         }
         else if (line.find("KNAPSACK DATA TYPE:") != string::npos)
         {
             line.erase(0, 20); // 删除"KNAPSACK DATA TYPE: "，保留背包数据类型
             test.set_knapsack_data_type(line);
-            cout << "KNAPSACK DATA TYPE:" << test.get_knapsack_data_type() << endl;
+            cerr << "KNAPSACK DATA TYPE:" << test.get_knapsack_data_type() << endl;
         }
         else if (line.find("DIMENSION:") != string::npos)
         {
             line.erase(0, 11); // 删除"DIMENSION: "，保留维度
             test.set_dimension(stoi(line));
-            cout << "DIMENSION:" << test.get_dimension() << endl;
+            cerr << "DIMENSION:" << test.get_dimension() << endl;
         }
         else if (line.find("NUMBER OF ITEMS:") != string::npos)
         {
             line.erase(0, 17); // 删除"NUMBER OF ITEMS: "，保留物品数量
             test.set_num_items(stoi(line));
-            cout << "NUMBER OF ITEMS:" << test.get_num_items() << endl;
+            cerr << "NUMBER OF ITEMS:" << test.get_num_items() << endl;
         }
         else if (line.find("CAPACITY OF KNAPSACK:") != string::npos)
         {
             line.erase(0, 22); // 删除"CAPACITY OF KNAPSACK: "，保留背包容量
             test.set_capacity_of_knapsack(stoi(line));
-            cout << "CAPACITY OF KNAPSACK:" << test.get_capacity_of_knapsack() << endl;
+            cerr << "CAPACITY OF KNAPSACK:" << test.get_capacity_of_knapsack() << endl;
         }
         else if (line.find("MIN SPEED:") != string::npos)
         {
-            // cout << "MIN SPEED find" << endl;
+            // cerr << "MIN SPEED find" << endl;
             line.erase(0, 11); // 删除"MIN SPEED: "，保留最小速度
             test.set_min_speed(stod(line));
-            cout << "MIN SPEED:" << test.get_min_speed() << endl;
+            cerr << "MIN SPEED:" << test.get_min_speed() << endl;
         }
         else if (line.find("MAX SPEED:") != string::npos)
         {
-            // cout << "MAX SPEED find" << endl;
+            // cerr << "MAX SPEED find" << endl;
             line.erase(0, 11); // 删除"MAX SPEED: "，保留最大速度
             test.set_max_speed(stod(line));
-            cout << "MAX SPEED:" << test.get_max_speed() << endl;
+            cerr << "MAX SPEED:" << test.get_max_speed() << endl;
         }
         else if (line.find("RENTING RATIO:") != string::npos)
         {
             line.erase(0, 14); // 删除"RENTING RATIO: "，保留租金比率
             test.set_renting_ratio(stod(line));
-            cout << "RENTING RATIO:" << test.get_renting_ratio() << endl;
+            cerr << "RENTING RATIO:" << test.get_renting_ratio() << endl;
         }
         else if (line.find("EDGE_WEIGHT_TYPE:") != string::npos)
         {
             line.erase(0, 18); // 删除"EDGE_WEIGHT_TYPE: "，保留边权类型
             test.set_edge_weight_type(line);
-            cout << "EDGE_WEIGHT_TYPE:" << test.get_edge_weight_type() << endl;
+            cerr << "EDGE_WEIGHT_TYPE:" << test.get_edge_weight_type() << endl;
         }
         else if (line.find("NODE_COORD_SECTION") != string::npos)
         {
-            cout << "NODE_COORD_SECTION (INDEX, X, Y): " << endl;
+            cerr << "NODE_COORD_SECTION (INDEX, X, Y): " << endl;
             while (getline(file, line))
             {
                 if (line.find("ITEMS SECTION") == string::npos)
@@ -96,12 +96,12 @@ int main()
                     NodeCoord temp = {x, y};
                     test.add_node_coords(temp);
 
-                    // cout << index << " " << temp.x << " " << temp.y << endl; // 输出节点坐标
+                    // cerr << index << " " << temp.x << " " << temp.y << endl; // 输出节点坐标
                     test_num_node_cord++;
                 }
                 else
                 {
-                    cout << "ITEMS SECTION (INDEX, PROFIT, WEIGHT, ASSIGNED NODE NUMBER): " << endl;
+                    cerr << "ITEMS SECTION (INDEX, PROFIT, WEIGHT, ASSIGNED NODE NUMBER): " << endl;
                     while (getline(file, line))
                     {
                         if (line.empty())
@@ -114,7 +114,7 @@ int main()
                         istringstream iss(line);
                         iss >> index >> profit >> weight >> assigned_node;
                         // 输出节点坐标
-                        // cout << index << " " << profit << " " << weight << " " << assigned_node << endl;
+                        // cerr << index << " " << profit << " " << weight << " " << assigned_node << endl;
                         test_num_item_section++;
                     }
                 }
@@ -123,8 +123,8 @@ int main()
     }
 
     file.close();
-    cout << "test_num_node_cord: " << test_num_node_cord << endl;
-    cout << "test_num_item_section: " << test_num_item_section << endl;
+    // cerr << "test_num_node_cord: " << test_num_node_cord << endl;
+    // cerr << "test_num_item_section: " << test_num_item_section << endl;
     test.print_node_coords();
 
     return 0;
