e08b5f8bb6de9f75c24753426f593256dee30720
YE GAO <sortsimilar@gmail.com>
2023-05-19 10:22:57 +0800

    Update DeepCopy.java

--- npbenchmark-main/travel_thief/TTP-forDistribution/src/ttp/Utils/DeepCopy.java
+++ npbenchmark-main/travel_thief/TTP-forDistribution/src/ttp/Utils/DeepCopy.java
@@ -1,4 +1,17 @@
 /*
+这个文件是一个Java类，它定义了一个名为DeepCopy的工具类，用于实现对象的深拷贝。
+深拷贝是指将一个对象的所有字段和引用的对象都复制一份，而不是只复制引用。这样可以避免修改原对象时影响到复制的对象。
+
+这个类有一个静态方法copy，它接受一个Object类型的参数，返回一个Object类型的结果。
+这个方法的实现是通过序列化和反序列化的方式来进行深拷贝。
+首先，它创建了一个FastByteArrayOutputStream对象，然后用ObjectOutputStream将参数对象写入到字节数组中。
+然后，它用ObjectInputStream从字节数组中读取一个新的对象，并返回。这个方法要求参数对象实现了Serializable接口，否则会抛出IOException或ClassNotFoundException异常。
+
+这个类还引入了一个FastByteArrayOutputStream类，它是一个继承自ByteArrayOutputStream的子类，用于提高序列化和反序列化的性能。
+它重写了getInputStream方法，返回一个内部类FastByteArrayInputStream的实例，而不是创建一个新的ByteArrayInputStream对象。
+ */
+
+/*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
