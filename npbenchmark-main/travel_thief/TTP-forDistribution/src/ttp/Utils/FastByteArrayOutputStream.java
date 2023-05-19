efa315cfdfe695f262dad33443883ef414581464
YE GAO <sortsimilar@gmail.com>
2023-05-19 15:54:48 +0800

    Update FastByteArrayOutputStream.java

--- npbenchmark-main/travel_thief/TTP-forDistribution/src/ttp/Utils/FastByteArrayOutputStream.java
+++ npbenchmark-main/travel_thief/TTP-forDistribution/src/ttp/Utils/FastByteArrayOutputStream.java
@@ -1,4 +1,18 @@
 /*
+这个代码文件是一个Java类，它继承了OutputStream类，用于输出字节数据。它的特点是不同步方法，也不复制数据到字节数组。它有以下几个主要的方法：
+
+构造方法：根据给定的初始大小，创建一个字节数组作为缓冲区。
+verifyBufferSize方法：检查缓冲区是否足够大，如果不够就扩容，并复制原来的数据。
+getSize方法：返回已经写入的数据的大小。
+getByteArray方法：返回包含写入数据的字节数组。
+write方法：重写了OutputStream类的write方法，将给定的字节或字节数组写入到缓冲区中。
+reset方法：将已经写入的数据的大小重置为0。
+getInputStream方法：返回一个FastByteArrayInputStream对象，用于读取写入的数据。
+
+这个类的作用是提高输出字节数据的效率，避免多余的同步和复制操作。它可以用于处理大量的字节数据，例如图片或音频等。
+ */
+
+/*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
