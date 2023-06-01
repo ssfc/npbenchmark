2e3fc9404845f55cc51a64ee6bb9871ba95f8f71
YE GAO <sortsimilar@gmail.com>
2023-06-01 16:54:19 +0800

    180	Add travel_thief.h and travel_theif.cpp

---
+++ npbenchmark-main/travel_thief/LKH-origin/SRC/travel_thief.cpp
@@ -0,0 +1,107 @@
+//
+// Created by take_ on 2023/6/1.
+//
+
+#include "travel_thief.h"
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+TravelThief::TravelThief():
+        num_cities(0),
+        num_items(0),
+        capacity(0),
+        min_speed(0.0),
+        max_speed(0.0),
+        renting_ratio(0.0),
+        object_value(0.0)
+{
+
+}
+
+TravelThief::~TravelThief()
+= default;
+
+void TravelThief::print_node_coords()
+{
+    cerr << "num node coords: " << city_coords.size() << endl;
+    for(int i=0; i < city_coords.size(); i++)
+    {
+        cerr << i << " " << city_coords[i].x << " " << city_coords[i].y << endl;
+    }
+}
+
+void TravelThief::compute_distances()
+{
+    distances.resize(city_coords.size());
+    for(int i=0; i < city_coords.size(); i++)
+    {
+        distances[i].resize(city_coords.size(), 0.0);
+    }
+
+    for(int i=0; i < city_coords.size(); i++)
+    {
+        for(int j=0; j < city_coords.size(); j++)
+        {
+            distances[i][j] = distances[j][i] = sqrt(
+                    (city_coords[i].x - city_coords[j].x) * (city_coords[i].x - city_coords[j].x)
+                    + (city_coords[i].y - city_coords[j].y) * (city_coords[i].y - city_coords[j].y));
+        }
+    }
+
+    cerr << "distance 0 and 1: " << distances[0][1] << endl;
+    cerr << "distance 1 and 2: " << distances[1][2] << endl;
+}
+
+void TravelThief::sample_solver()
+{
+    problem_name = "sample name";
+    knapsack_data_type = "sample knapsack type";
+    num_cities = 4;
+    num_items = 6;
+
+    capacity = 3; // capacity
+    min_speed = 0.1;
+    max_speed = 1;
+    renting_ratio = 1;
+    edge_weight_type = "sample edge type";
+
+    // sample distances;
+    distances.resize(num_cities);
+    for(auto & distance : distances)
+    {
+        distance.resize(num_cities, 0.0);
+    }
+    distances[0][1] = distances[1][0] = 5;
+    distances[0][2] = distances[2][0] = 6;
+    distances[0][3] = distances[3][0] = 6;
+    distances[1][2] = distances[2][1] = 5;
+    distances[1][3] = distances[3][1] = 6;
+    distances[2][3] = distances[3][2] = 4;
+
+    // sample items;
+    items.resize(num_items);
+    items[0] = {20, 2, 1};
+    items[1] = {30, 3, 1};
+    items[2] = {100, 3,2};
+    items[3] = {40, 1,2};
+    items[4] = {40, 1,2};
+    items[5] = {20, 2,3};
+
+    double speed_capacity_ratio = (max_speed - min_speed) / capacity;
+    cerr << "speed capacity ratio: " << speed_capacity_ratio << endl;
+
+    // object value of tour (0, 1, 3, 2) and object (0, 0, 0, 1, 1, 0)
+    object_value = 40 + 40 - 1 * (6/(1-0.3*2) + 5/1.0 + 6/1.0 + 4/1.0);
+    cerr << "object value: " << object_value << endl;
+}
+
+void TravelThief::print_items()
+{
+    cerr << "num items: " << items.size() << endl;
+    for(int i=0;i<items.size();i++)
+    {
+        cerr << i << " " << items[i].profit << " " << items[i].weight << " " << items[i].assigned_node << endl;
+    }
+}
---
+++ npbenchmark-main/travel_thief/LKH-origin/SRC/travel_thief.h
@@ -0,0 +1,125 @@
+//
+// Created by take_ on 2023/6/1.
+//
+
+#ifndef LKH_TRAVEL_THIEF_H
+#define LKH_TRAVEL_THIEF_H
+
+#include <string>
+#include <utility>
+#include <vector>
+
+struct NodeCoord{
+    int x;
+    int y;
+};
+
+struct Item{
+    int profit;
+    int weight;
+    int assigned_node;
+};
+
+class TravelThief{
+    std::string problem_name;
+    std::string knapsack_data_type;
+    int num_cities;
+    int num_items;
+    int capacity;
+    double min_speed;
+    double max_speed;
+    double renting_ratio;
+    std::string edge_weight_type;
+    std::vector<NodeCoord> city_coords;
+    std::vector<std::vector<double>> distances;
+    std::vector<Item> items;
+    std::vector<std::vector<int>> city_contained_items;  // city_contained_items[i] are list of items in city i
+    double object_value;
+
+public:
+    TravelThief();
+    ~TravelThief();
+
+    void set_problem_name(std::string input_problem_name) {
+        problem_name = std::move(input_problem_name);
+    }
+    std::string get_problem_name() {
+        return problem_name;
+    }
+
+    void set_knapsack_data_type(std::string input_knapsack_data_type) {
+        knapsack_data_type = std::move(input_knapsack_data_type);
+    }
+    std::string get_knapsack_data_type() {
+        return knapsack_data_type;
+    }
+
+    void set_dimension(int input_dimension) {
+        num_cities = input_dimension;
+    }
+    [[nodiscard]] int get_dimension() const {
+        return num_cities;
+    }
+
+    void set_num_items(int input_num_items) {
+        num_items = input_num_items;
+    }
+    [[nodiscard]] int get_num_items() const {
+        return num_items;
+    }
+
+    void set_capacity_of_knapsack(int input_num_items) {
+        capacity = input_num_items;
+    }
+    [[nodiscard]] int get_capacity_of_knapsack() const {
+        return capacity;
+    }
+
+    void set_min_speed(double input_min_speed) {
+        min_speed = input_min_speed;
+    }
+    [[nodiscard]] double get_min_speed() const {
+        return min_speed;
+    }
+
+    void set_max_speed(double input_max_speed) {
+        max_speed = input_max_speed;
+    }
+    [[nodiscard]] double get_max_speed() const {
+        return max_speed;
+    }
+
+    void set_renting_ratio(double input_max_speed) {
+        renting_ratio = input_max_speed;
+    }
+    [[nodiscard]] double get_renting_ratio() const {
+        return renting_ratio;
+    }
+
+    void set_edge_weight_type(std::string input_edge_weight_type) {
+        edge_weight_type = std::move(input_edge_weight_type);
+    }
+    [[nodiscard]] std::string get_edge_weight_type() const {
+        return edge_weight_type;
+    }
+
+    void add_node_coords(NodeCoord this_node_coord)
+    {
+        city_coords.push_back(this_node_coord);
+    }
+
+    void print_node_coords();
+
+    void add_item(Item this_item)
+    {
+        items.push_back(this_item);
+    }
+
+    void compute_distances();
+
+    void sample_solver();
+
+    void print_items();
+};
+
+#endif //LKH_TRAVEL_THIEF_H
