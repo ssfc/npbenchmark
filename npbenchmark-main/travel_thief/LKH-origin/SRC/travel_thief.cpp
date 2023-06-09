33ce26158bac34aa077062cbcad074b234ab5855
YE GAO <sortsimilar@gmail.com>
2023-06-09 19:14:44 +0800

    init_rand

--- npbenchmark-main/travel_thief/LKH-origin/SRC/travel_thief.cpp
+++ npbenchmark-main/travel_thief/LKH-origin/SRC/travel_thief.cpp
@@ -26,6 +26,9 @@ TravelThief::TravelThief():
         tour_length_computed(0.0),
         total_traveling_time(0.0)
 {
+    int input_seed = 1;
+    init_rand(input_seed); // initialize random generator;
+
     string filename = "../data/a280_n279_bounded-strongly-corr_01.ttp";
     // string filename = "../data/a280_n1395_uncorr-similar-weights_05.ttp";
     // string filename = "../data/fnl4461_n4460_bounded-strongly-corr_01.ttp";
