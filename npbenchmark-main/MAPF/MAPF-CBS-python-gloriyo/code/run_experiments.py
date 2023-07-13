3dcc89c8b232b979395c7676cc4e538bade6346a
YE GAO <sortsimilar@gmail.com>
2023-07-13 20:18:07 +0800

    update

--- npbenchmark-main/MAPF/MAPF-CBS-python-gloriyo/code/run_experiments.py
+++ npbenchmark-main/MAPF/MAPF-CBS-python-gloriyo/code/run_experiments.py
@@ -2,12 +2,11 @@
 import argparse
 import glob
 from pathlib import Path
-from cbs_basic import CBSSolver # original cbs with standard/disjoint splitting
+from cbs_basic import CBSSolver  # original cbs with standard/disjoint splitting
 
 # cbs with different improvements
-from icbs_cardinal_bypass import ICBS_CB_Solver # only cardinal dectection and bypass
-from icbs_complete import ICBS_Solver # all improvements including MA-CBS
-
+from icbs_cardinal_bypass import ICBS_CB_Solver  # only cardinal dectection and bypass
+from icbs_complete import ICBS_Solver  # all improvements including MA-CBS
 
 from independent import IndependentSolver
 from prioritized import PrioritizedPlanningSolver
@@ -17,6 +16,7 @@ from single_agent_planner import get_sum_of_cost
 HLSOLVER = "CBS"
 
 LLSOLVER = "a_star"
+
 
 def print_mapf_instance(my_map, starts, goals):
     print('Start locations')
@@ -91,7 +91,6 @@ if __name__ == '__main__':
                         help='The solver to use (one of: {a_star,pea_star,epea_star}), defaults to ' + str(LLSOLVER))
     args = parser.parse_args()
 
-
     result_file = open("results.csv", "w", buffering=1)
 
     # node_results_file = open("nodes-cleaned.csv", "w", buffering=1)
@@ -99,9 +98,7 @@ if __name__ == '__main__':
     nodes_gen_file = open("nodes-gen-cleaned.csv", "w", buffering=1)
     nodes_exp_file = open("nodes-exp-cleaned.csv", "w", buffering=1)
 
-
     if args.batch:
-        
         input_instance = sorted(glob.glob("instances/test*"))
     else:
         input_instance = sorted(glob.glob(args.instance))
@@ -110,7 +107,6 @@ if __name__ == '__main__':
 
         print("***Import an instance***")
 
-        
         print(file)
         my_map, starts, goals = import_mapf_instance(file)
         print_mapf_instance(my_map, starts, goals)
@@ -131,7 +127,7 @@ if __name__ == '__main__':
         elif args.hlsolver == "ICBS_CB":
             print("***Run ICBS with CB***")
             cbs = ICBS_CB_Solver(my_map, starts, goals)
- 
+
 
         elif args.hlsolver == "ICBS":
             print("***Run ICBS***")
@@ -146,8 +142,6 @@ if __name__ == '__main__':
             # else:
             #     raise BaseException('No solutions')
 
-
-
         # elif args.solver == "Independent":
         #     print("***Run Independent***")
         #     solver = IndependentSolver(my_map, starts, goals)
@@ -160,15 +154,13 @@ if __name__ == '__main__':
         else:
             raise RuntimeError("Unknown solver!")
 
-
-
         solution = cbs.find_solution(args.disjoint, args.llsolver)
 
         if solution is not None:
             # print(solution)
             paths, nodes_gen, nodes_exp = [solution[i] for i in range(3)]
             if paths is None:
-                raise BaseException('No solutions')  
+                raise BaseException('No solutions')
         else:
             raise BaseException('No solutions')
 
@@ -177,8 +169,6 @@ if __name__ == '__main__':
 
         nodes_gen_file.write("{},{}\n".format(file, nodes_gen))
         nodes_exp_file.write("{},{}\n".format(file, nodes_exp))
-
-
 
         if not args.batch:
             print("***Test paths on a simulation***")
