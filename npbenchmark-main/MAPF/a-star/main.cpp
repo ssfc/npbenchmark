30819509021c60e67bf2846700255b78b4f63765
YE GAO <sortsimilar@gmail.com>
2023-07-17 12:17:10 +0800

    Update main.cpp

--- npbenchmark-main/MAPF/a-star/main.cpp
+++ npbenchmark-main/MAPF/a-star/main.cpp
@@ -97,8 +97,7 @@ class AStar
 {
 private:
 public:
-    // A Function to find the shortest path betwee a given source cell to a destination cell according
-    // to A* Search Algorithm
+    // find the shortest path between a given source cell to a destination cell 
     static void aStarSearch(int grid[][COL], Pair src, Pair dest);
 };
 
