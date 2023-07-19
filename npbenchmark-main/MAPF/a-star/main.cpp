5ff05e6de5f9114aa73c3853037b329f0b4aa395
YE GAO <sortsimilar@gmail.com>
2023-07-19 17:22:41 +0800

    Update main.cpp

--- npbenchmark-main/MAPF/a-star/main.cpp
+++ npbenchmark-main/MAPF/a-star/main.cpp
@@ -12,7 +12,7 @@ int main()
     /* Description of the Grid-
     1--> The cell is not blocked
     0--> The cell is blocked */
-    std::vector<std::vector<int>>  test_map
+    std::vector<std::vector<int>> test_map
             = { { 1, 0, 1, 1, 1, 1, 0, 1, 1, 1 },
                 { 1, 1, 1, 0, 1, 1, 1, 0, 1, 1 },
                 { 1, 1, 1, 0, 1, 1, 0, 1, 0, 1 },
