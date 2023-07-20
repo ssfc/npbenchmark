55a5abdbce00bb91587f8dd87746e4eb338fa383
YE GAO <sortsimilar@gmail.com>
2023-07-20 15:58:26 +0800

    Update draw_path.py

--- npbenchmark-main/MAPF/a-star/draw_path.py
+++ npbenchmark-main/MAPF/a-star/draw_path.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import time
 
+
 def draw_map(canvas, map_data):
     block_size = 40  # 每个方块的大小
     for row in range(len(map_data)):
@@ -10,20 +11,25 @@ def draw_map(canvas, map_data):
             else:
                 color = 'black'  # 障碍物为黑色
 
-            canvas.create_rectangle(col*block_size, row*block_size, (col+1)*block_size, (row+1)*block_size, fill=color)
+            canvas.create_rectangle(col * block_size, row * block_size, (col + 1) * block_size, (row + 1) * block_size,
+                                    fill=color)
+
 
 def draw_path(canvas, path_data):
     block_size = 40
     for i in range(len(path_data)):
         row, col = path_data[i]
-        canvas.create_oval(col * block_size + 5, row * block_size + 5, col * block_size + block_size - 5, row * block_size + block_size - 5, fill='red')
+        canvas.create_oval(col * block_size + 5, row * block_size + 5, col * block_size + block_size - 5,
+                           row * block_size + block_size - 5, fill='red')
         canvas.update()
         time.sleep(0.5)  # 路径点之间的时间间隔
+
 
 def main():
     # 读取map.txt内容
     with open('map.txt', 'r') as file:
-        map_data = [list(line.strip()) for line in file.readlines()]
+        rows, cols = map(int, file.readline().strip().split())  # Read the number of rows and columns
+        map_data = [list(file.readline().strip()) for _ in range(rows)]  # Read each row and convert it to a list
 
     # 读取path.txt内容
     with open('path.txt', 'r') as file:
@@ -40,6 +46,7 @@ def main():
     draw_path(canvas, path_data)
 
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
