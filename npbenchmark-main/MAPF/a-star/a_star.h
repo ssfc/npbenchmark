0e3c8e43aeabd88b6bbf7bf4c69a6335d8d1a227
YE GAO <sortsimilar@gmail.com>
2023-07-21 10:59:59 +0800

    Update a_star.h

--- npbenchmark-main/MAPF/a-star/a_star.h
+++ npbenchmark-main/MAPF/a-star/a_star.h
@@ -49,10 +49,10 @@ struct OpenNode
 // A structure to hold the necessary parameters
 struct Cell
 {
-    double f; // f = g + h; fScore[n] represents our current best guess as to
-    // how cheap a path could be from start to finish if it goes through n.
     double g; // cost of the cheapest path from start to n currently known
     double h; // h(n) estimates the cost to reach goal from node n
+    double f; // f = g + h; fScore[n] represents our current best guess as to
+    // how cheap a path could be from start to finish if it goes through n.
 
     // Row and Column index of its parent
     // Note that 0 <= i <= num_row-1 & 0 <= j <= num_columns-1
