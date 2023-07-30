1bd3c3bfd740df326a16f0396a6e01cc66c7f4b0
YE GAO <sortsimilar@gmail.com>
2023-07-30 13:45:18 +0800

    update

--- npbenchmark-main/MAPF/MAPF-CBS-python-gloriyo/code/cbs_basic.py
+++ npbenchmark-main/MAPF/MAPF-CBS-python-gloriyo/code/cbs_basic.py
@@ -134,20 +134,20 @@ def disjoint_splitting(collision):
 
 def paths_violate_constraint(constraint, paths):
     assert constraint['positive'] is True
-    rst = []
+    result = []  # store index of conflicted path
     for i in range(len(paths)):
         if i == constraint['agent']:
             continue
-        curr = get_location(paths[i], constraint['timestep'])
-        prev = get_location(paths[i], constraint['timestep'] - 1)
+        current_location = get_location(paths[i], constraint['timestep'])
+        prev_location = get_location(paths[i], constraint['timestep'] - 1)
         if len(constraint['loc']) == 1:  # vertex constraint
-            if constraint['loc'][0] == curr:
-                rst.append(i)
+            if constraint['loc'][0] == current_location:
+                result.append(i)
         else:  # edge constraint
-            if constraint['loc'][0] == prev or constraint['loc'][1] == curr \
-                    or constraint['loc'] == [curr, prev]:
-                rst.append(i)
-    return rst
+            if constraint['loc'][0] == prev_location or constraint['loc'][1] == current_location \
+                    or constraint['loc'] == [current_location, prev_location]:
+                result.append(i)
+    return result
 
 
 class CBSSolver(object):
