f11fd47aa26044de36ffd3c31e4435d598fdb010
SSFC <sortsimilar@gmail.com>
2023-08-02 21:49:24 +0800

    add new

---
+++ npbenchmark-main/MAPF/MAPF-CBS-cpp-mine/cmake-build-debug/CMakeFiles/a_star.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a_star.dir/a_star.cpp.obj"
+  "CMakeFiles/a_star.dir/a_star.cpp.obj.d"
+  "CMakeFiles/a_star.dir/cbs.cpp.obj"
+  "CMakeFiles/a_star.dir/cbs.cpp.obj.d"
+  "CMakeFiles/a_star.dir/main.cpp.obj"
+  "CMakeFiles/a_star.dir/main.cpp.obj.d"
+  "a_star.exe"
+  "a_star.exe.manifest"
+  "a_star.pdb"
+  "liba_star.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a_star.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
