cb717d4ad95710ecbc0a720e8b2dd8d724c66e8f
YE GAO <sortsimilar@gmail.com>
2023-08-22 19:37:08 +0800

    在 C++ 中，更好的方式是使用成员初始化列表（member initialization list）来初始化成员变量，

--- npbenchmark-main/MAPF/MAPF-CBS-cpp-mine/low_level.cpp
+++ npbenchmark-main/MAPF/MAPF-CBS-cpp-mine/low_level.cpp
@@ -3,7 +3,10 @@
 //
 #include "low_level.h"
 
-
+LowLevel::LowLevel(std::vector<std::vector<int>> &input_map):
+          map(input_map)
+{
+}
 
 
 
--- npbenchmark-main/MAPF/MAPF-CBS-cpp-mine/low_level.h
+++ npbenchmark-main/MAPF/MAPF-CBS-cpp-mine/low_level.h
@@ -5,8 +5,17 @@
 #ifndef DRAW_PATH_PY_LOW_LEVEL_H
 #define DRAW_PATH_PY_LOW_LEVEL_H
 
+#include <vector>
+
 class LowLevel
 {
+    int num_rows;
+    int num_columns;
+    std::vector<std::vector<int>> map;
+
+
+public:
+    LowLevel(std::vector<std::vector<int>>& input_map); // constructor
 
 };
 
