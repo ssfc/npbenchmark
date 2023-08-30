84ff418a24b2c489b7721c94a1064cad0c0c0fca
YE GAO <sortsimilar@gmail.com>
2023-08-30 12:46:32 +0800

    update

--- npbenchmark-main/MAPF/MAPF-CBS-python-gloriyo/code/learn_grammar.py
+++ npbenchmark-main/MAPF/MAPF-CBS-python-gloriyo/code/learn_grammar.py
@@ -17,8 +17,7 @@ class Triple:
 x = [(1,2,3), (2,1,4), (3,4,5), (4,3,6), (5,6,7), (1,2,5), (4,3,2), (2,1,3)]
 y = [Triple(a,b,c) for a,b,c in x]
 heapq.heapify(y)
-z = [(t.a, t.b, t.c) for t in y]
-print(z)
-# 输出：[(1, 2, 3), (1, 2, 5), (2, 1, 4), (2, 1, 3), (3, 4, 5), (5, 6, 7), (4, 3, 6), (4 ,3 ,2)]
 
-# 输出：[<__main__.Triple object at 0x7f8c9c0d7a90>, <__main__.Triple object at 0x7f8c9c0d7b80>, <__main__.Triple object at 0x7f8c9c0d7a60>, <__main__.Triple object at 0x7f8c9c0d7bb0>, <__main__.Triple object at 0x7f8c9c0d7ac0>, <__main__.Triple object at 0x7f8c9c0d7af0>, <__main__.Triple object at 0x7f8c9c0d7b50>, <__main__.Triple object at 0x7f8c9c0d7be0>]
+while y:
+    popped_element = heapq.heappop(y)
+    print(popped_element.a, popped_element.b, popped_element.c)
