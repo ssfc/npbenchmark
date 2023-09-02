3389f10538db913748dd9bc3ab6bb6aa5d2a0b1e
YE GAO <sortsimilar@gmail.com>
2023-09-02 13:54:43 +0800

    update

--- npbenchmark-main/MAPF/MAPF-CBS-python-gloriyo/code/cbs_basic.py
+++ npbenchmark-main/MAPF/MAPF-CBS-python-gloriyo/code/cbs_basic.py
@@ -473,7 +473,7 @@ class CBSSolver(object):
         # Implement A1 LINE 5
         # while OPEN not empty do
         iteration = 0  # Iteration需要走144个，重复实验结果一样。
-        while len(self.open_list) > 0 and iteration < 2000:
+        while len(self.open_list) > 0 and iteration < 2:
             print("iteration: ", iteration)
             # if self.num_of_generated > 50000:
             #     print('reached maximum number of nodes. Returning...')
