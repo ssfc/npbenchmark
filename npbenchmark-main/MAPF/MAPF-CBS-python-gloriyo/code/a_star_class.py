ef10ce02763202d5d4775142ec04b382475dc61a
YE GAO <sortsimilar@gmail.com>
2023-09-03 15:50:03 +0800

    416	用[0]代替self.agents的循环。

--- npbenchmark-main/MAPF/MAPF-CBS-python-gloriyo/code/a_star_class.py
+++ npbenchmark-main/MAPF/MAPF-CBS-python-gloriyo/code/a_star_class.py
@@ -78,7 +78,7 @@ class A_Star(object):
 
         # FILTER BY INDEX FOR STARTS AND GOALS AND HEURISTICS
         # 因为是CBS, 所以下面这些都是只有1个成员的list.
-        self.starts = [input_starts[this_agent] for this_agent in self.agents]
+        self.starts = [input_starts[self.agents[0]]]
         # print("self.starts: ", self.starts)
         self.goals = [input_goals[this_agent] for this_agent in self.agents]
         self.heuristics = [input_heuristics[this_agent] for this_agent in self.agents]
